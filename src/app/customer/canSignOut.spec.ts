f0fe892cdf01f20e7194ce177f84f8ec2dac5f64
David Chin <davidchin.yf@gmail.com>
2019-08-09 09:30:13 +1000

    feat(customer): CHECKOUT-4223 Add customer information component

---
+++ src/app/customer/canSignOut.spec.ts
@@ -0,0 +1,30 @@
+import { getCheckout, getCheckoutWithPayments } from '../checkout/checkouts.mock';
+
+import canSignOut from './canSignOut';
+import { getCustomer, getGuestCustomer } from './customers.mock';
+
+describe('canSignOut()', () => {
+    it('returns false if customer is guest', () => {
+        const customer = getGuestCustomer();
+        const checkout = getCheckout();
+
+        expect(canSignOut(customer, checkout))
+            .toEqual(false);
+    });
+
+    it('returns true if customer is signed in', () => {
+        const customer = getCustomer();
+        const checkout = getCheckout();
+
+        expect(canSignOut(customer, checkout))
+            .toEqual(true);
+    });
+
+    it('returns false if customer started with payment method that does not allow sign-out', () => {
+        const customer = getCustomer();
+        const checkout = getCheckoutWithPayments();
+
+        expect(canSignOut(customer, checkout))
+            .toEqual(false);
+    });
+});
