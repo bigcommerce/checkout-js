1767ec725a99e107451faa8af34780d9932d92da
David Chin <davidchin.yf@gmail.com>
2019-08-09 09:29:15 +1000

    feat(customer): CHECKOUT-4223 Add returning customer form

---
+++ src/app/customer/LoginForm.tsx
@@ -0,0 +1,115 @@
+import { withFormik, FormikProps } from 'formik';
+import React, { FunctionComponent } from 'react';
+import { object, string } from 'yup';
+
+import { preventDefault } from '../common/dom';
+import { TranslatedHtml, TranslatedString } from '../language';
+import { withLanguage, WithLanguageProps } from '../locale';
+import { Alert, AlertType } from '../ui/alert';
+import { Button, ButtonVariant } from '../ui/button';
+import { Fieldset, Form, Legend } from '../ui/form';
+
+import mapErrorMessage from './mapErrorMessage';
+import { EMAIL_REGEXP } from './validationPatterns';
+import EmailField from './EmailField';
+import PasswordField from './PasswordField';
+
+export interface LoginFormProps {
+    canCancel?: boolean;
+    createAccountUrl: string;
+    email?: string;
+    forgotPasswordUrl: string;
+    isSigningIn?: boolean;
+    signInError?: Error;
+    onCancel?(): void;
+    onChangeEmail?(email: string): void;
+    onSignIn(data: LoginFormValues): void;
+}
+
+export interface LoginFormValues {
+    email: string;
+    password: string;
+}
+
+const LoginForm: FunctionComponent<LoginFormProps & WithLanguageProps & FormikProps<LoginFormValues>> = ({
+    canCancel,
+    createAccountUrl,
+    forgotPasswordUrl,
+    isSigningIn,
+    language,
+    onCancel,
+    onChangeEmail,
+    signInError,
+}) => (
+    <Form
+        className="checkout-form"
+        testId="checkout-customer-returning"
+        id="checkout-customer-returning"
+    >
+        <Fieldset legend={
+            <Legend hidden>
+                <TranslatedString id="customer.returning_customer_text" />
+            </Legend>
+        }>
+            { signInError && <Alert
+                testId="customer-login-error-message"
+                type={ AlertType.Error }
+            >
+                { mapErrorMessage(signInError, key => language.translate(key)) }
+            </Alert> }
+
+            <p>
+                <TranslatedHtml
+                    data={ { url: createAccountUrl } }
+                    id="customer.create_account_to_continue_text"
+                />
+            </p>
+
+            <EmailField onChange={ onChangeEmail } />
+
+            <PasswordField forgotPasswordUrl={ forgotPasswordUrl } />
+
+            <div className="form-actions">
+                <Button
+                    testId="customer-continue-button"
+                    id="checkout-customer-continue"
+                    isLoading={ isSigningIn }
+                    variant={ ButtonVariant.Primary }
+                    type="submit"
+                >
+                    <TranslatedString id="customer.sign_in_action" />
+                </Button>
+
+                { canCancel && <a
+                    className="button optimizedCheckout-buttonSecondary"
+                    data-test="customer-cancel-button"
+                    href="#"
+                    id="checkout-customer-cancel"
+                    onClick={ preventDefault(onCancel) }
+                >
+                    <TranslatedString id="common.cancel_action" />
+                </a> }
+            </div>
+        </Fieldset>
+    </Form>
+);
+
+export default withLanguage(withFormik<LoginFormProps & WithLanguageProps, LoginFormValues>({
+    mapPropsToValues: ({
+        email = '',
+    }) => ({
+        email,
+        password: '',
+    }),
+    handleSubmit: (values, { props: { onSignIn } }) => {
+        onSignIn(values);
+    },
+    validationSchema: ({ language }: LoginFormProps & WithLanguageProps) => object({
+        email: string()
+            .max(256)
+            .matches(EMAIL_REGEXP, language.translate('customer.email_invalid_error'))
+            .required(language.translate('customer.email_required_error')),
+        password: string()
+            .required(language.translate('customer.password_required_error')),
+    }),
+})(LoginForm));
---
+++ src/app/customer/PasswordField.tsx
@@ -0,0 +1,47 @@
+import React, { FunctionComponent } from 'react';
+
+import { TranslatedString } from '../language';
+import { FormField, TextInput } from '../ui/form';
+import { IconEye, IconEyeSlash } from '../ui/icon';
+import { Toggle } from '../ui/toggle';
+
+export interface PasswordField {
+    forgotPasswordUrl: string;
+}
+
+const PasswordField: FunctionComponent<PasswordField> = ({
+    forgotPasswordUrl,
+}) => (
+    <FormField
+        labelContent={ <TranslatedString id={'customer.password_label' } /> }
+        name="password"
+        footer={
+            <a
+                data-test="forgot-password-link"
+                href={ forgotPasswordUrl }
+                target="_blank"
+            >
+                <TranslatedString id="customer.forgot_password_action" />
+            </a>
+        }
+        input={ props =>
+            <Toggle openByDefault={ false }>
+                { ({ isOpen, toggle }) => (
+                    <div className="form-field-password">
+                        <TextInput
+                            { ...props.field }
+                            id={ props.field.name }
+                            additionalClassName="form-input--withIcon"
+                            type={ isOpen ? 'text' : 'password' }
+                        />
+                        <a href="#" className="form-toggle-password form-input-icon" onClick={ toggle } >
+                            { isOpen ? <IconEye /> : <IconEyeSlash /> }
+                        </a>
+                    </div>
+                ) }
+            </Toggle>
+        }
+    />
+);
+
+export default PasswordField;
