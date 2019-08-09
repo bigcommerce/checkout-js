49633b2ff917d0d98a7f97d25810ece88cbb0c41
Luis Sanchez <luis.sanchez@bigcommerce.com>
2019-08-09 12:23:40 +1000

    feat(common): CHECKOUT-4223 Add GoogleAddressAutocomplete component

---
+++ src/app/address/googleAutocomplete/index.ts
@@ -0,0 +1,5 @@
+export * from './googleAutocompleteTypes';
+export { default as mapToAddress } from './mapToAddress';
+export { default as GoogleAutocompleteScriptLoader } from './GoogleAutocompleteScriptLoader';
+export { default as GoogleAutocomplete } from './GoogleAutocomplete';
+export { default as GoogleAutocompleteFormField } from './GoogleAutocompleteFormField';
---
+++ src/app/address/googleAutocomplete/mapToAddress.spec.ts
@@ -0,0 +1,35 @@
+import { getCountries } from '../../geography/countries.mock';
+
+import mapToAddress from './mapToAddress';
+import { getGoogleAutocompletePlaceMock } from './__mocks__/googleAutocompleteResult';
+
+describe('mapToAddress()', () => {
+    it('returns a partial address with hydrated province', () => {
+        const googlePlace = getGoogleAutocompletePlaceMock();
+        const countries = getCountries();
+
+        const address = mapToAddress(googlePlace, countries);
+
+        expect(address).toMatchObject({
+            city: 'Ultimo PT (l)',
+            countryCode: 'AU',
+            postalCode: '2007',
+            stateOrProvince: 'New South Wales',
+            stateOrProvinceCode: 'NSW',
+        });
+    });
+
+    it('returns a partial address with province code when no countries are passed', () => {
+        const googlePlace = getGoogleAutocompletePlaceMock();
+
+        const address = mapToAddress(googlePlace);
+
+        expect(address).toMatchObject({
+            city: 'Ultimo PT (l)',
+            countryCode: 'AU',
+            postalCode: '2007',
+            stateOrProvince: 'NSW',
+            stateOrProvinceCode: '',
+        });
+    });
+});
