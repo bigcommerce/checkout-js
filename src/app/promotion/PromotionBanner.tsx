82560635429f52dc7fd3065c590f1a7735c077b3
David Chin <davidchin.yf@gmail.com>
2019-08-09 11:42:56 +1000

    feat(checkout): CHECKOUT-4223 Add component for rendering list of promotion banners

---
+++ src/app/promotion/PromotionBanner.tsx
@@ -0,0 +1,28 @@
+import DOMPurify from 'dompurify';
+import React, { FunctionComponent } from 'react';
+
+import { Alert, AlertType } from '../ui/alert';
+import { IconTag } from '../ui/icon';
+
+export interface PromotionBannerProps {
+    message: string;
+}
+
+const PromotionBanner: FunctionComponent<PromotionBannerProps> = ({
+    message,
+}) => (
+    <Alert
+        additionalClassName="optimizedCheckout-discountBanner"
+        icon={ <IconTag /> }
+        type={ AlertType.Info }
+    >
+        <span
+            data-test="promotion-banner-message"
+            dangerouslySetInnerHTML={ {
+                __html: DOMPurify.sanitize(message),
+            } }
+        />
+    </Alert>
+);
+
+export default PromotionBanner;
---
+++ src/app/promotion/index.ts
@@ -0,0 +1,1 @@
+export { default as PromotionBannerList } from './PromotionBannerList';
