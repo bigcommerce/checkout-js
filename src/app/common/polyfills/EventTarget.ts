fd59c7d7b8964664a1ef4b1b5d4bbdad06141648
David Chin <davidchin.yf@gmail.com>
2019-08-21 11:32:51 +1000

    chore(common): CHECKOUT-4351 Remove unused dependencies

--- src/app/common/polyfills/EventTarget.ts
+++
@@ -1,3 +0,0 @@
-import EventTarget from '@ungap/event-target';
-
-(window as any).EventTarget = EventTarget;
--- types/ungap.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '@ungap/event-target' {
-    export default {} as any;
-}
-
-declare module '@ungap/custom-event' {
-    export default {} as void;
-}
