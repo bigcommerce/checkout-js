1b308b96ada2b7f602141b0e5b014981cff7d05a
Carlos Lopez <clopez@intersysconsulting.com>
2019-08-22 15:05:42 -0500

    feat(payment): INT-1737 Update component to support APMs

--- src/app/payment/paymentMethod/AdyenPaymentMethod.tsx
+++ src/app/payment/paymentMethod/AdyenPaymentMethod.tsx
@@ -1,3 +1,4 @@
+import { CardComponentOptions } from '@bigcommerce/checkout-sdk';
 import React, { FunctionComponent } from 'react';
 import { Omit } from 'utility-types';
 
@@ -5,25 +6,47 @@ import HostedWidgetPaymentMethod, { HostedWidgetPaymentMethodProps } from './Hos
 
 export type AdyenPaymentMethodProps = Omit<HostedWidgetPaymentMethodProps, 'containerId' | 'hideContentWhenSignedOut'>;
 
+export interface AdyenOptions {
+    scheme: CardComponentOptions;
+    bcmc: CardComponentOptions;
+}
+
+export enum AdyenMethodType {
+    scheme = 'scheme',
+    bcmc = 'bcmc',
+}
+
 const AdyenPaymentMethod: FunctionComponent<AdyenPaymentMethodProps> = ({
     initializePayment,
+    method,
     ...rest
-}) => (
-    <HostedWidgetPaymentMethod
+}) => {
+    const containerId = method.id + '-adyen-component-field';
+    const component: AdyenMethodType = AdyenMethodType[method.method as keyof typeof AdyenMethodType];
+
+    const adyenOptions: AdyenOptions = {
+        [AdyenMethodType.scheme]: {
+            hasHolderName: true,
+        },
+        [AdyenMethodType.bcmc]: {
+            hasHolderName: true,
+        },
+    };
+
+    return <HostedWidgetPaymentMethod
         {...rest}
-        containerId="adyen-component-field"
+        containerId= { containerId }
         hideContentWhenSignedOut
+        method={ method }
         initializePayment={ options => initializePayment({
             ...options,
             adyenv2: {
-                containerId: 'adyen-component-field',
-                options: {
-                    hasHolderName: true,
-                },
+                containerId,
+                options: adyenOptions[component],
                 threeDS2ChallengeWidgetSize: '01',
             },
         })}
-    />
-);
+    />;
+};
 
 export default AdyenPaymentMethod;
