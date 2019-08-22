caf3a8348e5523c6e4a5c407412c7babadc5f955
David Chin <davidchin.yf@gmail.com>
2019-08-22 16:59:28 +1000

    perf(customer): CHECKOUT-4272 Stop passing arrow functions into components in customer section

--- src/app/customer/EmailField.tsx
+++ src/app/customer/EmailField.tsx
@@ -1,4 +1,5 @@
-import React, { FunctionComponent } from 'react';
+import { FieldProps } from 'formik';
+import React, { useCallback, FunctionComponent } from 'react';
 
 import { TranslatedString } from '../locale';
 import { FormField, TextInput } from '../ui/form';
@@ -9,20 +10,22 @@ export interface EmailFieldProps {
 
 const EmailField: FunctionComponent<EmailFieldProps>  = ({
     onChange,
-}) => (
-    <FormField
+}) => {
+    const renderInput = useCallback((props: FieldProps) => (
+        <TextInput
+            { ...props.field }
+            autoComplete={ props.field.name }
+            id={ props.field.name }
+            type="email"
+        />
+    ), []);
+
+    return <FormField
         name="email"
         labelContent={ <TranslatedString id="customer.email_label" /> }
         onChange={ onChange }
-        input={ props =>
-            <TextInput
-                { ...props.field }
-                autoComplete={ props.field.name }
-                id={ props.field.name }
-                type="email"
-            />
-        }
-    />
-);
+        input={ renderInput }
+    />;
+};
 
 export default EmailField;
