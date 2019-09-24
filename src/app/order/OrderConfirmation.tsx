40bc95f92e9783a3abacb8cf2abb876ecbbb043a
Ankur Agarwal <ankur.agarwal@bigcommerce.com>
2019-09-24 14:12:11 +1000

    fix(checkout): CHECKOUT-2070 Fixed linting errors

--- src/app/order/OrderConfirmation.tsx
+++ src/app/order/OrderConfirmation.tsx
@@ -129,10 +129,10 @@ class OrderConfirmation extends Component<
 
                         { paymentInstructions && <OrderConfirmationSection>
                             <div
-                                data-test="payment-instructions"
                                 dangerouslySetInnerHTML={ {
                                     __html: DOMPurify.sanitize(paymentInstructions),
                                 } }
+                                data-test="payment-instructions"
                             />
                         </OrderConfirmationSection> }
 
@@ -202,9 +202,9 @@ class OrderConfirmation extends Component<
                                             { ...mapToOrderSummarySubtotalsProps(order) }
                                             headerLink={ <PrintLink className="modal-header-link cart-modal-link" /> }
                                             lineItems={ order.lineItems }
-                                            total={ order.orderAmount }
-                                            storeCurrency={ currency }
                                             shopperCurrency={ shopperCurrency }
+                                            storeCurrency={ currency }
+                                            total={ order.orderAmount }
                                         />
                                     </LazyContainer>;
 
@@ -216,9 +216,9 @@ class OrderConfirmation extends Component<
                                                 headerLink={ <PrintLink /> }
                                                 { ...mapToOrderSummarySubtotalsProps(order) }
                                                 lineItems={ order.lineItems }
-                                                total={ order.orderAmount }
-                                                storeCurrency={ currency }
                                                 shopperCurrency={ shopperCurrency }
+                                                storeCurrency={ currency }
+                                                total={ order.orderAmount }
                                             />
                                         </LazyContainer>
                                     </aside>;
