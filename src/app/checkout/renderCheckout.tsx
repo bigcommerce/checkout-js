4fa5c1763a8700c1818889ecaad5e3a78ffefdbb
David Chin <davidchin.yf@gmail.com>
2019-09-27 15:08:24 +1000

    fix(common): CHECKOUT-4418 Fix import path of external ESM modules that are conditionally required

--- src/app/checkout/renderCheckout.tsx
+++ src/app/checkout/renderCheckout.tsx
@@ -13,7 +13,20 @@ export default function renderCheckout({
     ...props
 }: RenderCheckoutOptions): void {
     const configuredPublicPath = configurePublicPath(publicPath);
+
+    // We want to use `require` here because we want to set up the public path
+    // first before importing the app component and its dependencies.
     const { default: CheckoutApp } = require('./CheckoutApp');
+
+    // We want to use `require` here because we only want to import the package
+    // in development mode.
+    if (process.env.NODE_ENV === 'development') {
+        const whyDidYouRender = require('@welldone-software/why-did-you-render');
+
+        whyDidYouRender(React, {
+            collapseGroups: true,
+        });
+    }
 
     ReactDOM.render(
         <CheckoutApp
--- src/app/index.ts
+++ src/app/index.ts
@@ -1,15 +1,2 @@
-import React from 'react';
-
-if (process.env.NODE_ENV === 'development') {
-    // We want to use `require` because we only want to import the package in
-    // development mode.
-    // tslint:disable-next-line
-    const { default: whyDidYouRender } = require('@welldone-software/why-did-you-render');
-
-    whyDidYouRender(React, {
-        collapseGroups: true,
-    });
-}
-
 export { default as renderCheckout } from '../app/checkout/renderCheckout';
 export { default as renderOrderConfirmation } from '../app/order/renderOrderConfirmation';
