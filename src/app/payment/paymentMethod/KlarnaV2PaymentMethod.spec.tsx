0ecb5a8f0f36e96f0a9443b275b0d5649be5ca98
rlopz <rodolfo.lopez@bigcommerce.com>
2019-10-15 14:42:31 -0500

    feat(checkout): INT-1434 Create KlarnaV2PaymentMethod to support Multi Option

---
+++ src/app/payment/paymentMethod/KlarnaV2PaymentMethod.spec.tsx
@@ -0,0 +1,89 @@
+import { createCheckoutService, CheckoutSelectors, CheckoutService, PaymentMethod } from '@bigcommerce/checkout-sdk';
+import { mount, ReactWrapper } from 'enzyme';
+import { Formik } from 'formik';
+import { noop } from 'lodash';
+import React, { FunctionComponent } from 'react';
+
+import { CheckoutProvider } from '../../checkout';
+import { getStoreConfig } from '../../config/config.mock';
+import { createLocaleContext, LocaleContext, LocaleContextType } from '../../locale';
+import { getPaymentMethod } from '../payment-methods.mock';
+
+import HostedWidgetPaymentMethod, { HostedWidgetPaymentMethodProps } from './HostedWidgetPaymentMethod';
+import { default as PaymentMethodComponent, PaymentMethodProps } from './PaymentMethod';
+import PaymentMethodId from './PaymentMethodId';
+
+describe('when using Klarna payment', () => {
+    let method: PaymentMethod;
+    let checkoutService: CheckoutService;
+    let checkoutState: CheckoutSelectors;
+    let defaultProps: PaymentMethodProps;
+    let localeContext: LocaleContextType;
+    let PaymentMethodTest: FunctionComponent<PaymentMethodProps>;
+
+    beforeEach(() => {
+        defaultProps = {
+            method: getPaymentMethod(),
+            onUnhandledError: jest.fn(),
+        };
+
+        checkoutService = createCheckoutService();
+        checkoutState = checkoutService.getState();
+        localeContext = createLocaleContext(getStoreConfig());
+        method = { ...getPaymentMethod(), id: 'pay_now', gateway: PaymentMethodId.Klarna };
+
+        jest.spyOn(checkoutState.data, 'getConfig')
+            .mockReturnValue(getStoreConfig());
+
+        jest.spyOn(checkoutService, 'deinitializePayment')
+            .mockResolvedValue(checkoutState);
+
+        jest.spyOn(checkoutService, 'initializePayment')
+            .mockResolvedValue(checkoutState);
+
+        PaymentMethodTest = props => (
+            <CheckoutProvider checkoutService={ checkoutService }>
+                <LocaleContext.Provider value={ localeContext }>
+                    <Formik
+                        initialValues={ {} }
+                        onSubmit={ noop }
+                    >
+                        <PaymentMethodComponent { ...props } />
+                    </Formik>
+                </LocaleContext.Provider>
+            </CheckoutProvider>
+        );
+    });
+
+    it('renders as hosted widget method', () => {
+        const container = mount(<PaymentMethodTest { ...defaultProps } method={ method } />);
+        const component: ReactWrapper<HostedWidgetPaymentMethodProps> = container.find(HostedWidgetPaymentMethod);
+
+        expect(component.props())
+            .toEqual(expect.objectContaining({
+                containerId: `${ method.id }Widget`,
+                deinitializePayment: expect.any(Function),
+                initializePayment: expect.any(Function),
+                method,
+            }));
+    });
+
+    it('initializes method with required config', () => {
+        const container = mount(<PaymentMethodTest { ...defaultProps } method={ method } />);
+        const component: ReactWrapper<HostedWidgetPaymentMethodProps> = container.find(HostedWidgetPaymentMethod);
+
+        component.prop('initializePayment')({
+            methodId: method.id,
+            gatewayId: method.gateway,
+        });
+
+        expect(checkoutService.initializePayment)
+            .toHaveBeenCalledWith(expect.objectContaining({
+                methodId: method.id,
+                gatewayId: method.gateway,
+                [`${ method.gateway }v2`]: {
+                    container: `#${ method.id }Widget`,
+                },
+            }));
+    });
+});
---
+++ src/app/payment/paymentMethod/KlarnaV2PaymentMethod.tsx
@@ -0,0 +1,28 @@
+import React, { useCallback, FunctionComponent } from 'react';
+import { Omit } from 'utility-types';
+
+import HostedWidgetPaymentMethod, { HostedWidgetPaymentMethodProps } from './HostedWidgetPaymentMethod';
+
+export type KlarnaPaymentMethodProps = Omit<HostedWidgetPaymentMethodProps, 'containerId'>;
+
+const KlarnaV2PaymentMethod: FunctionComponent<KlarnaPaymentMethodProps> = ({
+    initializePayment,
+    ...rest
+}) => {
+    const initializeKlarnaV2Payment = useCallback(options => initializePayment(
+        {
+            ...options,
+            klarnav2: {
+                container: `#${options.methodId}Widget`,
+            },
+        }
+    ), [initializePayment]);
+
+    return <HostedWidgetPaymentMethod
+        { ...rest }
+        containerId={ `${rest.method.id}Widget` }
+        initializePayment={ initializeKlarnaV2Payment }
+    />;
+};
+
+export default KlarnaV2PaymentMethod;
