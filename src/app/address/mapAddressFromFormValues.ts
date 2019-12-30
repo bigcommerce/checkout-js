10cb6cfec3ada5f7d53528dd49d9b9ba38d84785
Luis Sanchez <luis.sanchez@bigcommerce.com>
2019-12-30 15:18:49 +1100

    fix(common): CHECKOUT-4606 Set right timezone in custom date field

--- src/app/address/mapAddressFromFormValues.ts
+++ src/app/address/mapAddressFromFormValues.ts
@@ -7,12 +7,25 @@ export default function mapAddressFromFormValues(formValues: AddressFormValues):
     const { customFields: customFieldsObject, ...address } = formValues;
     const customFields: Array<{fieldId: string; fieldValue: string}> = [];
 
-    forIn(customFieldsObject, (value, key) =>
+    forIn(customFieldsObject, (value, key) => {
+        let fieldValue: string;
+
+        if (isDate(value)) {
+            const dateValue = new Date(value);
+            // We want the field value to match the exact date that the user entered.
+            // However, when we call toISOString, it will converted to UTC and the user timezone could affect the
+            // UTC representation by ±1 day. To avoid this, subtract the time offset from the date.
+            dateValue.setMinutes(dateValue.getMinutes() - dateValue.getTimezoneOffset());
+            fieldValue = dateValue.toISOString().slice(0, 10);
+        } else {
+            fieldValue = value;
+        }
+
         customFields.push({
             fieldId: key,
-            fieldValue: isDate(value) ? value.toISOString().slice(0, 10) : value,
-        })
-    );
+            fieldValue,
+        });
+    });
 
     return {
         ...address,
