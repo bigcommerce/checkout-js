5e2ca88698a15090533566ea73c40a41fa282e55
Victor Parra <victor.parra@bigcommerce.com>
2020-03-18 19:15:35 -0600

    fix(payment): INT-2427 Use ExpiryDate while paying with a vaulted Bancontact card rather than CVV

--- src/app/payment/paymentMethod/AdyenV2CardValidation.spec.tsx
+++ src/app/payment/paymentMethod/AdyenV2CardValidation.spec.tsx
@@ -8,23 +8,35 @@ describe('AdyenV2CardValidation', () => {
     let AdyenV2CardValidationTest: FunctionComponent<AdyenV2CardValidationProps>;
 
     beforeEach(() => {
-        defaultProps = {
-            verificationFieldsContainerId: 'container',
-            shouldShowNumberField: true,
-        };
-
         AdyenV2CardValidationTest = props => (
             <AdyenV2CardValidation { ...props } />
         );
     });
 
-    it('renders Adyen V2 secured fields', () => {
+    it('renders Adyen V2 Card Number and CVV fields', () => {
+        defaultProps = {
+            paymentMethodType: 'scheme',
+            shouldShowNumberField: true,
+            verificationFieldsContainerId: 'container',
+        };
+
         const container = mount(<AdyenV2CardValidationTest { ...defaultProps } />);
 
-        expect(container.props())
-            .toEqual(expect.objectContaining({
-                verificationFieldsContainerId: 'container',
-                shouldShowNumberField: true,
-            }));
+        const field = container.find('[id="encryptedSecurityCode"]');
+
+        expect(field).toHaveLength(1);
+    });
+
+    it('renders Adyen V2 Card Number and Expiry Date fields', () => {
+        defaultProps = {
+            paymentMethodType: 'bcmc',
+            shouldShowNumberField: true,
+            verificationFieldsContainerId: 'container',
+        };
+        const container = mount(<AdyenV2CardValidationTest { ...defaultProps } />);
+
+        const field = container.find('[id="encryptedExpiryDate"]');
+
+        expect(field).toHaveLength(1);
     });
 });
