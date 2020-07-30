264afc8eb68fc505e1116da76242b8c30659d8b9
Lee Dunkley <lee.dunkley@bigcommerce.com>
2020-07-30 13:06:46 +1000

    chore(payment): PAYMENTS-5513 fix whitespace issues

--- src/app/payment/creditCard/StoreCreditCardFieldset.tsx
+++ src/app/payment/creditCard/StoreCreditCardFieldset.tsx
@@ -3,7 +3,7 @@ import React, { useState, FC } from 'react';
 import CreditCardStorageField from './CreditCardStorageField';
 import CreditCardStoreAsDefaultField from './CreditCardStoreAsDefaultField';
 
-const StoreCreditCardFieldset: FC<{showSetAsDefault: boolean}> = ({showSetAsDefault}) => {
+const StoreCreditCardFieldset: FC<{ showSetAsDefault: boolean }> = ({ showSetAsDefault }) => {
     const [saveIsChecked, setSaveIsChecked] = useState(false);
     const trackSaveCheckedStatus = (isChecked: boolean) => {
         setSaveIsChecked(isChecked);
