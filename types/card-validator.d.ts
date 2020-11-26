d63923884c47065d15edc578ea5584b2ff50b046
Julio Abarca <julio.abarca@bigcommerce.com>
2020-11-26 12:55:50 -0600

    feat(checkout): INT-3475 [Checkout.com] Mada card logo doesn't highlight on checkout

--- types/card-validator.d.ts
+++ types/card-validator.d.ts
@@ -3,13 +3,26 @@ import { CreditCardType as OriginalCreditCardType, CreditCardTypeInfo as Origina
 
 // Merge @types/card-validator with missing methods
 declare module 'card-validator' {
+    type CardBrand =
+        | "american-express"
+        | "diners-club"
+        | "discover"
+        | "jcb"
+        | "maestro"
+        | "mastercard"
+        | "unionpay"
+        | "visa"
+        | "mada";
+
     interface CreditCardTypeInfo extends OriginalCreditCardTypeInfo {
+        type?: CardBrand;
         patterns?: Array<number | [number, number]>;
     }
 
     interface CreditCardType extends OriginalCreditCardType {
         getTypeInfo(type: string): CreditCardTypeInfo;
         updateCard(type: string, updates: Partial<CreditCardTypeInfo>): void;
+        addCard(config: Partial<CreditCardTypeInfo>): void;
     }
 
     export const creditCardType: CreditCardType;
