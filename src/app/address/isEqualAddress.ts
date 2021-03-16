37020ecc02873f6fe0a727e3d6054991a1c141e2
Luis Sanchez <capsula4@yahoo.com.ar>
2021-03-16 16:22:17 +1100

    fix(shipping): CHECKOUT-5618 Check state code for address equality

--- src/app/address/isEqualAddress.ts
+++ src/app/address/isEqualAddress.ts
@@ -12,13 +12,27 @@ export default function isEqualAddress(address1?: ComparableAddress, address2?: 
     return isEqual(
         normalizeAddress(address1),
         normalizeAddress(address2)
-    );
+    ) && isSameState(address1, address2);
+}
+
+function isSameState(address1: ComparableAddress, address2: ComparableAddress): boolean {
+    if (address1.stateOrProvince && address1.stateOrProvince === address2.stateOrProvince) {
+        return true;
+    }
+
+    if (address1.stateOrProvinceCode && address1.stateOrProvinceCode === address2.stateOrProvinceCode) {
+        return true;
+    }
+
+    return address1.stateOrProvince === address2.stateOrProvince &&
+        address1.stateOrProvinceCode === address2.stateOrProvinceCode;
 }
 
 function normalizeAddress(address: ComparableAddress) {
     const ignoredFields: ComparableAddressFields[] = [
         'id',
         'shouldSaveAddress',
+        'stateOrProvince',
         'stateOrProvinceCode',
         'type',
         'email',
