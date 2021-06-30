36793ecc5e477418752b8098d92a38f3d94965fc
Axel Malagon <axel.malagon@bigcommerce.com>
2021-06-30 18:43:06 -0500

    feat(payment): INT-3946 added locale from browser on masterpass SRC

--- src/app/payment/paymentMethod/MasterpassPaymentMethod.tsx
+++ src/app/payment/paymentMethod/MasterpassPaymentMethod.tsx
@@ -2,7 +2,7 @@ import { PaymentInitializeOptions } from '@bigcommerce/checkout-sdk';
 import React, { useCallback, useMemo, FunctionComponent } from 'react';
 import { Omit } from 'utility-types';
 
-import { withLanguage, WithLanguageProps } from '../../locale';
+import { getLanguageService, withLanguage, WithLanguageProps } from '../../locale';
 
 import WalletButtonPaymentMethod, { WalletButtonPaymentMethodProps } from './WalletButtonPaymentMethod';
 
@@ -20,8 +20,43 @@ const MasterpassPaymentMethod: FunctionComponent<MasterpassPaymentMethodProps & 
         },
     }), [initializePayment]);
 
+    const formatLocale = useCallback((localeLanguage: string) => {
+        const supportedLocales: {[language: string]: string[]} = {es: ['es_es', 'es_mx', 'es_pe', 'es_co', 'es_ar', 'es_cl'],
+            en: ['en_us', 'en_gb', 'en_ca', 'en_es', 'en_fr', 'en_ie', 'en_sg', 'en_au', 'en_nz', 'en_my', 'en_hk', 'en_th', 'en_ae', 'en_sa', 'en_qa', 'en_kw', 'en_za'],
+            pt: ['pt_br'],
+            zu: ['zu_za'],
+            ar: ['ar_sa', 'ar_ae', 'ar_qa', 'ar_kw'],
+            zh: ['zh_sg', 'zh_hk'],
+            ms: ['ms_my'],
+            uk: ['uk_ua'],
+            sv: ['sv_se'],
+            hr: ['hr_hr'],
+            pl: ['pl_pl'],
+            nl: ['nl_be'],
+            it: ['it_it'],
+            de: ['de_de'],
+            fr: ['fr_fr', 'fr_ca']};
+        let formatedLocale = localeLanguage.replace('-', '_').toLowerCase();
+        const regex = formatedLocale.match(/^([a-z]{2})((?:\_)([a-z]{2}))?$/);
+        if (regex && regex[3]) {
+            if (regex[1] in supportedLocales) {
+                formatedLocale = supportedLocales[regex[1]].includes(regex[0]) ? formatedLocale : supportedLocales[regex[1]][0];
+            } else {
+                formatedLocale = 'en_us';
+            }
+        } else if (regex) {
+            formatedLocale = regex[1] in supportedLocales ? supportedLocales[regex[1]][0] : 'en_us';
+        } else {
+            formatedLocale = 'en_us';
+        }
+
+        return formatedLocale;
+    }, []);
+
     const { config: { testMode }, initializationData: { checkoutId, isMasterpassSrcEnabled } } = rest.method;
-    const locale = navigator.language.replace('-', '_');
+
+    const locale = formatLocale(getLanguageService().getLocale());
+
     const signInButtonLabel = useMemo(() => (
         <img
             alt={ language.translate('payment.masterpass_name_text') }
