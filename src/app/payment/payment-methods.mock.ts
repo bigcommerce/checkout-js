85085dbdb6e076d2b3642fe5b900c0426d181c5a
Vitaliy Koshovyi <vitaliy.koshovyi@bigcommerce.com>
2021-07-06 14:24:41 +0300

    feat(payment): PAYPAL-1057 added check of showOnlyOnMobileDevices APM property for Venmo ppcommerce integration

--- src/app/payment/payment-methods.mock.ts
+++ src/app/payment/payment-methods.mock.ts
@@ -25,3 +25,32 @@ export function getPaymentMethod(): PaymentMethod {
         type: 'PAYMENT_TYPE_API',
     };
 }
+
+export function getMobilePaymentMethod(): PaymentMethod {
+    return {
+        id: 'authorizenetMobile',
+        gateway: undefined,
+        logoUrl: '',
+        method: 'credit-card',
+        supportedCards: [
+            'VISA',
+            'AMEX',
+            'MC',
+        ],
+        initializationData: {
+            showOnlyOnMobileDevices: true,
+        },
+        config: {
+            displayName: 'Authorizenet',
+            cardCode: true,
+            enablePaypal: undefined,
+            hasDefaultStoredInstrument: false,
+            helpText: '',
+            is3dsEnabled: undefined,
+            isVisaCheckoutEnabled: undefined,
+            merchantId: undefined,
+            testMode: false,
+        },
+        type: 'PAYMENT_TYPE_API',
+    };
+}
