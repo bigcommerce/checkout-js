de40aec5e90e34c2c1ad3d3006b698fb5b960341
Vitaliy Koshovyi <vitaliy.koshovyi@bigcommerce.com>
2021-07-06 14:24:41 +0300

    feat(payment): PAYPAL-1057 added check of showOnlyOnMobileDevices APM property for Venmo ppcommerce integration

--- src/app/common/utility/index.ts
+++ src/app/common/utility/index.ts
@@ -4,3 +4,4 @@ export { default as retry, RetryOptions } from './retry';
 export { default as parseAnchor } from './parseAnchor';
 export { default as isRecord } from './isRecord';
 export { default as isRecordContainingKey } from './isRecordContainingKey';
+export { default as isMobile } from './isMobile';
--- src/app/payment/paymentMethod/PaymentMethodList.spec.tsx
+++ src/app/payment/paymentMethod/PaymentMethodList.spec.tsx
@@ -5,7 +5,7 @@ import { noop, range } from 'lodash';
 import React, { FunctionComponent } from 'react';
 
 import { Checklist, ChecklistItem } from '../../ui/form';
-import { getPaymentMethod } from '../payment-methods.mock';
+import { getMobilePaymentMethod, getPaymentMethod } from '../payment-methods.mock';
 
 import PaymentMethodList, { PaymentMethodListProps } from './PaymentMethodList';
 
@@ -34,6 +34,28 @@ describe('PaymentMethodList', () => {
 
         expect(component.find(ChecklistItem))
             .toHaveLength(methods.length);
+    });
+
+    it('renders mobile payment methods as checklist on mobile device', () => {
+        methods = [getMobilePaymentMethod(), getPaymentMethod()];
+
+        // @ts-ignore: setter for userAgent is defined in jest-setup.ts
+        window.navigator.userAgent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1';
+        const component = mount(<PaymentMethodListTest methods={ methods } />);
+
+        expect(component.find(ChecklistItem))
+            .toHaveLength(2);
+    });
+
+    it('does not renders mobile payment methods as checklist on desktop', () => {
+        methods = [getMobilePaymentMethod(), getPaymentMethod()];
+
+        // @ts-ignore: setter for userAgent is defined in jest-setup.ts
+        window.navigator.userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36';
+        const component = mount(<PaymentMethodListTest methods={ methods } />);
+
+        expect(component.find(ChecklistItem))
+            .toHaveLength(1);
     });
 
     it('configures each item in list with value', () => {
