eb80c6318b0cd0b047196bbd4d3f1b774921214f
Lee Dunkley <lee.dunkley@bigcommerce.com>
2021-08-17 17:00:17 +1000

    fix(payment): PAYMENTS-7214 fix PPSDK initialisation strategy casing

--- src/app/payment/paymentMethod/PPSDKPaymentMethod/initializationComponentMap.ts
+++ src/app/payment/paymentMethod/PPSDKPaymentMethod/initializationComponentMap.ts
@@ -5,5 +5,5 @@ import { NoUI } from './NoUI';
 type ComponentMap = Record<string, FunctionComponent>;
 
 export const initializationComponentMap: ComponentMap = {
-    NONE: NoUI,
+    none: NoUI,
 };
