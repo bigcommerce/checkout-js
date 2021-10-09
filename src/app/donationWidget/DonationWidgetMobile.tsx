5ddd0fa471642f66e3fd76af7f38de3bd7decbb5
Thomas Francis <thomas.francis@fotf.org>
2021-10-08 22:26:24 -0400

    Fixed merge commit; added widgetIsMobile

--- src/app/donationWidget/DonationWidgetMobile.tsx
+++ src/app/donationWidget/DonationWidgetMobile.tsx
@@ -3,11 +3,7 @@ import React, { FunctionComponent } from 'react';
 import DonationWidget from './DonationWidget';
 
 const DonationWidgetMobile: FunctionComponent = () => (
-<<<<<<< HEAD
     <div data-test="cart" id="donationWidget">
-=======
-    <div className="cart optimizedCheckout-orderSummary" data-test="cart" id="donationWidget">
->>>>>>> fix: fix some linting errors
         <div className="cart-header">
             <h3 className="cart-title optimizedCheckout-headingSecondary">
                 { 'Donate to Focus on the Family' }
@@ -15,13 +11,10 @@ const DonationWidgetMobile: FunctionComponent = () => (
         </div>
 
         <div className="cart-section optimizedCheckout-orderSummary-cartSection">
-            <DonationWidget />
+            <DonationWidget
+                { ...{ widgetIsMobile: false } }
+            />
         </div>
-<<<<<<< HEAD
     </div>);
-=======
-    </div>
-);
->>>>>>> fix: fix some linting errors
 
 export default DonationWidgetMobile;
