a076d3d75a9975780399052dd8073aa3a6d3d613
Peng Zhou <88361607+bc-peng@users.noreply.github.com>
2021-12-21 17:00:03 +1100

    feat(checkout): CHECKOUT-6248 Display Error When Tax Service Unavailable (#761)
    * feat(checkout): CHECKOUT-6174 Display Error When Tax Service Unavailable
    
    This reverts commit fe32b40fb44d064d2fa81a98a3434ec11cb88ed5, reversing
    changes made to b92df442fb9d9415cb22537ad691bfff05ff7781.
    
    feat(checkout): CHECKOUT-6174 Display Error When Tax Service Unavailable
    
    feat(checkout): CHECKOUT-6174 Display Error When Tax Service Unavailable
    
    feat(checkout): CHECKOUT-6174 Display Error When Tax Service Unavailable
    
    CHECKOUT-6174 Display Error When Tax Service Unavailable
    
    feat(payment): INT-4897 Humm - Show Humm as payment method (#722)
    
    chore(checkout): ADYEN-320 bump sdk version
    
    "chore(release): 1.184.0 [skip ci]"
    
    fix(checkout): CHECKOUT-6215 Display long descriptions of shipping methods properly (#764)
    
    * fix(checkout): CHECKOUT-6215 Long descriptions of shipping methods stretch page
    
    * fix(checkout): CHECKOUT-6215 Display long descriptions of shipping methods properly
    
    * fix(checkout): CHECKOUT-6215 Display long descriptions of shipping methods properly
    
    * fix(checkout): CHECKOUT-6215 Display long descriptions of shipping methods properly
    
    "chore(release): 1.184.1 [skip ci]"
    
    chore(payment): BOLT-100 bump checkout-sdk-js version
    
    "chore(release): 1.184.2 [skip ci]"
    
    refactor(payment): INT-1866 Update Visa Checkout to Visa SRC (#762)
    
    chore(payment): BOLT-109 bump up checkout-sdk-js version
    
    "chore(release): 1.184.3 [skip ci]"
    
    feat(checkout): CHECKOUT-6059 Add and update language translations (#768)
    
    "chore(release): 1.185.0 [skip ci]"
    
    feat(payment): INT-5000 Populate onUnhandledError correctly
    
    feat(payment): INT-5000 Adding DigitalRiver custom error for translations
    
    test(payment): INT-4855 increase code coverage on CheckoutcomCustomFormFields
    
    chore(payment): INT-4893 INT-4926 Bump checkout-sdk
    
    "chore(release): 1.186.0 [skip ci]"
    
    feat(checkout): CHECKOUT-6174 Display Error When Tax Service Unavailable
    
    feat(checkout): CHECKOUT-6174 Display Error When Tax Service Unavailable
    
    Co-Authored-By: Animesh Singh <animeshsingh1987@gmail.com>
    
    * feat(checkout): CHECKOUT-6174 Display Error When Tax Service Unavailable
    
    Co-authored-by: Animesh Singh <animeshsingh1987@gmail.com>

--- src/app/payment/mapSubmitOrderErrorMessage.spec.ts
+++ src/app/payment/mapSubmitOrderErrorMessage.spec.ts
@@ -113,6 +113,12 @@ describe('mapSubmitOrderErrorMessage()', () => {
         expect(message).toEqual(translate('payment.payment_method_error', { message: 'payment error message' }));
     });
 
+    it('returns correct message when tax provider is unavailable', () => {
+        const message = mapSubmitOrderErrorMessage({ type: 'tax_provider_unavailable' }, translate, false);
+
+        expect(message).toEqual(translate('payment.tax_provider_unavailable'));
+    });
+
     describe('When bigpay request error and localization enabled,', () => {
         it('returns correct translated message when one error is received', () => {
             const message = mapSubmitOrderErrorMessage(
