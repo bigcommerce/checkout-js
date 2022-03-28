eacf8512117baa380ccf36b1084a5b3303d27aeb
Peng <88361607+bc-peng@users.noreply.github.com>
2022-03-28 16:19:08 +1100

    feat(checkout): CHECKOUT-6340 HAR demo

--- tests/paymentIntegrations/BigpayCreditcard.spec.ts
+++ tests/paymentIntegrations/BigpayCreditcard.spec.ts
@@ -1,20 +1,17 @@
-import { chromium, expect, test, Page } from '@playwright/test';
+import { chromium, expect, test } from '@playwright/test';
 import { Polly } from '@pollyjs/core';
 import FSPersister from '@pollyjs/persister-fs';
+import path from 'path';
 import { PlaywrightAdapter } from 'polly-adapter-playwright';
 
-// dirty hack for root certificate issue
+// dirty hack for root certificate issue during recording HAR
 // https://stackoverflow.com/questions/31673587/error-unable-to-verify-the-first-certificate-in-nodejs
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-const path = require('path');
-
-test.use({
-    headless: false,
-    viewport: { width: 1000, height: 1000 },
-});
-
-// test.beforeEach(async ({ page }) => {
+// Uncommmnet to continue in a headed browser
+// test.use({
+//     headless: false,
+//     viewport: { width: 1000, height: 1000 },
 // });
 
 test.describe('Checkout', () => {
@@ -24,10 +21,9 @@ test.describe('Checkout', () => {
         const browser = await chromium.launch();
         const page = await browser.newPage();
 
-        // register the playwright adapter so it's accessible by all future polly instances
+        // Setup PollyJS
         Polly.register(PlaywrightAdapter);
         Polly.register(FSPersister);
-
         const polly = new Polly('checkout', {
             mode: 'replay',
             // logLevel: 'info',
@@ -38,35 +34,6 @@ test.describe('Checkout', () => {
                 },
             },
             persister: 'fs',
-        });
-        // polly.server.any().on('beforeReplay', (req, recording) => {
-        //     console.log("REPLAY!!!!!!!", recording);
-        // });
-        polly.server.any().on('request', req => {
-            req.url = req.url.replace('http://localhost:8080/api/public/v1/orders/payments', 'https://bigpay.service.bcdev/api/public/v1/orders/payments');
-            req.url = req.url.replace('http://localhost:8080', 'https://my-dev-store-745516528.store.bcdev');
-        });
-        polly.server.post('http://localhost:8080/api/public/v1/orders/payments').intercept((req, res) => {
-            res.status(201);
-            res.setHeaders({
-                'Access-Control-Allow-Methods': 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Max-Age': '1728000',
-                'Cache-Control': 'max-age=0, private, must-revalidate',
-                'Content-Type': 'application/json',
-                ETag: 'W/"7b5cabf18eb298d44dcca610c3127b86"',
-                'Referrer-Policy': 'strict-origin-when-cross-origin',
-            });
-            res.json({
-                status: 'ok',
-                three_ds_result: {
-                    acs_url: null,
-                    payer_auth_request: null,
-                    merchant_data: null,
-                    callback_url: null,
-                },
-                errors: [],
-            });
         });
         polly.configure({
             persisterOptions: {
@@ -81,18 +48,38 @@ test.describe('Checkout', () => {
             },
         });
 
-        // polly.server.any().on('beforeResponse', (req, res) => {
-        //     console.log(req, res);
-        // });
+        // Intercept Bigpay, not sure why (only) this cannot be replayed, always seeing a 405 error.
+        polly.server.post('http://localhost:8080/api/public/v1/orders/payments').intercept((_, res) => {
+            res.status(200);
+            res.setHeaders({
+                'Access-Control-Allow-Methods': 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
+                'Access-Control-Allow-Origin': '*',
+            });
+            res.json({
+                status: 'ok',
+                three_ds_result: {
+                    acs_url: null,
+                    payer_auth_request: null,
+                    merchant_data: null,
+                    callback_url: null,
+                },
+                errors: [],
+            });
+        });
+        // Convert localhost URL to dev store, so HAR file can replay them
+        polly.server.any().on('request', req => {
+            req.url = req.url.replace('http://localhost:8080', 'https://my-dev-store-745516528.store.bcdev');
+        });
 
+        // Serving static files through Playwright
         await page.route('/', route => route.fulfill( {status: 200, path: './tests/_support/index.html' } ));
         await page.route('**/checkout/payment/hosted-field?**', route => route.fulfill( {status: 200, path: './tests/_support/hostedField.html' } ));
         await page.route('**/order-confirmation', route => route.fulfill( {status: 200, path: './tests/_support/orderConfirmation.html' } ));
+        await page.route('**/ablebrewingsystem4.1647253530.190.285.jpg?c=1', route => route.fulfill( {status: 200, path: './tests/_support/product.gif' } ));
 
-        // await page.goto('https://my-dev-store-745516528.store.bcdev/');
+        // Playwright scripts
         await page.goto('http://localhost:8080/');
-        // await page.pause();
-
+        // await page.goto('https://my-dev-store-745516528.store.bcdev/');
         // Click [data-test="card-86"] >> text=Add to Cart
         // await page.locator('[data-test="card-86"] >> text=Add to Cart').click();
         // assert.equal(page.url(), 'https://my-dev-store-745516528.store.bcdev/cart.php?suggest=ae7a82e0-fd10-4df5-9dc3-f23a7f5c5aa2');
@@ -125,12 +112,8 @@ test.describe('Checkout', () => {
         await page.locator('[data-test="postCodeInput-text"]').fill('10028');
         // Click text=Continue
         await page.locator('text=Continue').click();
-
-        // await page.pause();
-
         // Click text=Test Payment ProviderVisaAmexMaster
         await page.locator('text=Test Payment ProviderVisaAmexMaster').click();
-
         // Click [aria-label="Credit Card Number"]
         await page.frameLocator('#bigpaypay-ccNumber iframe').locator('[aria-label="Credit Card Number"]').click();
         // Fill [aria-label="Credit Card Number"]
@@ -148,15 +131,15 @@ test.describe('Checkout', () => {
         // Fill [aria-label="CVV"]
         await page.frameLocator('#bigpaypay-ccCvv iframe').locator('[aria-label="CVV"]').fill('111');
         // Click text=Place Order
-
         await page.locator('text=Place Order').click();
-
         await page.locator('.orderConfirmation').waitFor({state: 'visible'});
 
+        // Assertion for this test
         await expect(page.locator('.orderConfirmation')).toContainText('Your order number is');
 
         await page.pause();
-        // cleanup
+
+        // Clean up
         await polly.stop();
         await page.close();
     });
