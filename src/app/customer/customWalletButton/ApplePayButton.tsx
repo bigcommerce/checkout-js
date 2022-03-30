df25f030d40be34904f39d8c24c1c6feab3e84e9
animesh1987 <animeshsingh1987@gmail.com>
2022-03-30 11:24:15 +1100

    fix(payment): CHECKOUT-6549 Add check if window has apple pay supported

--- src/app/customer/customWalletButton/ApplePayButton.tsx
+++ src/app/customer/customWalletButton/ApplePayButton.tsx
@@ -2,6 +2,7 @@ import { CustomerInitializeOptions } from '@bigcommerce/checkout-sdk';
 import React, { useCallback, useContext, FunctionComponent } from 'react';
 
 import { navigateToOrderConfirmation } from '../../checkout';
+import { isApplePayWindow } from '../../common/utility';
 import { LocaleContext } from '../../locale';
 import CheckoutButton, { CheckoutButtonProps } from '../CheckoutButton';
 
@@ -21,6 +22,10 @@ const ApplePayButton: FunctionComponent<CheckoutButtonProps> = ({
             onPaymentAuthorize: navigateToOrderConfirmation,
         },
     }), [initialize, localeContext, onError, rest.containerId]);
+
+    if (!isApplePayWindow(window)) {
+        return null;
+    }
 
     return <CheckoutButton initialize={ initializeOptions } { ...rest } />;
 };
