fc73f221dbaf8e67a6b729bb6554c4b06d7c037f
Tharaa <tharaa.elmorssi@bigcommerce.com>
2022-04-20 10:04:04 +1000

    feat(payment): PAYMENTS-7788 Add icons for Crypto instruments

--- src/app/payment/creditCard/CreditCardIcon.tsx
+++ src/app/payment/creditCard/CreditCardIcon.tsx
@@ -1,6 +1,8 @@
 import React, { memo, FunctionComponent } from 'react';
 
-import { IconCardAmex, IconCardCarnet, IconCardCB, IconCardDankort, IconCardDinersClub, IconCardDiscover, IconCardElo, IconCardHipercard, IconCardJCB, IconCardMada, IconCardMaestro, IconCardMastercard, IconCardTroy, IconCardUnionPay, IconCardVisa, IconSize } from '../../ui/icon';
+import { IconSize } from '../../ui/icon';
+
+import { getPaymentMethodIconComponent } from './mapFromPaymentMethodCardType';
 
 export interface CreditCardIconProps {
     cardType?: string;
@@ -15,55 +17,9 @@ const CreditCardIcon: FunctionComponent<CreditCardIconProps> = ({
         testId: `credit-card-icon-${cardType || 'default'}`,
     };
 
-    switch (cardType) {
-    case 'american-express':
-        return <IconCardAmex { ...iconProps } />;
+    const IconComponent = getPaymentMethodIconComponent(cardType);
 
-    case 'carnet':
-        return <IconCardCarnet { ...iconProps } />;
-
-    case 'cb':
-        return <IconCardCB { ...iconProps } />;
-
-    case 'dankort':
-        return <IconCardDankort { ...iconProps } />;
-
-    case 'diners-club':
-        return <IconCardDinersClub { ...iconProps } />;
-
-    case 'discover':
-        return <IconCardDiscover { ...iconProps } />;
-
-    case 'elo':
-        return <IconCardElo { ...iconProps } />;
-
-    case 'hiper':
-        return <IconCardHipercard { ...iconProps } />;
-
-    case 'jcb':
-        return <IconCardJCB { ...iconProps } />;
-
-    case 'mada':
-        return <IconCardMada { ...iconProps } />;
-
-    case 'maestro':
-        return <IconCardMaestro { ...iconProps } />;
-
-    case 'mastercard':
-        return <IconCardMastercard { ...iconProps } />;
-
-    case 'troy':
-        return <IconCardTroy { ...iconProps } />;
-
-    case 'unionpay':
-        return <IconCardUnionPay { ...iconProps } />;
-
-    case 'visa':
-        return <IconCardVisa { ...iconProps } />;
-
-    default:
-        return <div className="cardIcon-icon cardIcon-icon--default icon icon--medium" />;
-    }
+    return IconComponent ? <IconComponent { ...iconProps } /> : <div className="cardIcon-icon cardIcon-icon--default icon icon--medium" />;
 };
 
 export default memo(CreditCardIcon);
