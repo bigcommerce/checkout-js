72859bc980c9d2b3dcdbbf5cd9b368ca276eadfa
Peng Zhou <88361607+bc-peng@users.noreply.github.com>
2022-07-20 11:43:42 +1000

    fix(checkout): CHECKOUT-0000 Update e2e tests for buy-now cart project (#927)
    * fix(checkout): CHECKOUT-0000 Update e2e tests for buy-now cart project
    
    * fix(checkout): CHECKOUT-0000 match all kinds of order-confirmation
    
    * fix(checkout): CHECKOUT-0000 update URL rules
    
    fix(checkout): CHECKOUT-0000 update URL

--- tests/sampleTests/sampletest.Afterpay.spec.ts
+++ tests/sampleTests/sampletest.Afterpay.spec.ts
@@ -3,7 +3,7 @@ import { test, PaymentStepAsGuestPreset, UseAUDPreset } from '../';
 test.describe('Sample Test Group', () => {
     test('Can see Afterpay AU on the payment step', async ({assertions, checkout}) => {
         // Testing environment setup
-        const storeUrl = 'https://my-dev-store-745516528.store.bcdev';
+        const storeUrl = 'https://my-dev-store-117450812.store.bcdev';
         await checkout.use(new UseAUDPreset(storeUrl));
         await checkout.use(new PaymentStepAsGuestPreset(storeUrl));
         await checkout.create('sample Afterpay AUD', storeUrl);
