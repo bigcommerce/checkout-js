d623f762a9d06596871d6a05c11dbfbdfe6a892a
Kohji Lincoln <klincoln@bolt.com>
2022-07-21 19:40:07 -0400

    add analytics module

---
+++ packages/core/src/app/checkout/AnalyticsEvents.ts
@@ -0,0 +1,32 @@
+
+let boltTracker: Window["BoltTrack"] = {
+  recordEvent: (event: string) => console.log("--bolt bigc--: NOOP ", event)
+}
+
+const eventLog: string[] = []
+
+export const AnalyticsEvents = {
+  init: () => {
+    console.log("--bolt bigc--: intializing Bolt-BigC analytics events")
+    if(window && window.BoltTrack) {
+      boltTracker = window.BoltTrack
+      console.log("--bolt bigc--: successfully assigned window BoltTrack")
+      // immediately send checkout start event
+      AnalyticsEvents.emitEvent("Checkout load success")
+    }
+  },
+  emitEvent: (eventName: string) => {
+    console.log("--bolt bigc--: emitting analytics event ", eventName);
+    if (boltTracker.recordEvent) {
+      const props: any = {
+        nextState: eventName
+      }
+      if (eventLog.length > 0) {
+        props.prevState = eventLog[0];
+      }
+      boltTracker.recordEvent("CheckoutFunnelTransition", props)
+      eventLog.push(eventName)
+      console.log("--bolt bigc--: successfully sent analytics event ", eventName);
+    }
+  }
+}
