d57a29760d4c02a2c3bae7dd108373c0967bd28f
Mauricio Sanchez <mauricio.sanchez@bigcommerce.com>
2022-08-15 11:34:18 -0500

    fix(payment): INT-6404 AmazonPayV2: Prevent button flickering

---
+++ packages/core/src/app/customer/customWalletButton/AmazonPayV2Button.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from 'react';
+
+import CheckoutButton, { CheckoutButtonProps } from '../CheckoutButton';
+
+const AmazonPayV2Button: FunctionComponent<CheckoutButtonProps> = props => (
+  <div className="AmazonPayContainer">
+    <CheckoutButton { ...props } />
+  </div>
+);
+
+export default AmazonPayV2Button;
