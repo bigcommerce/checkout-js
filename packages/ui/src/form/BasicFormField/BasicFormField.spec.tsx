68a33d709302f6cec555deb2d6ab4182a0a01695
animesh1987 <animeshsingh1987@gmail.com>
2022-10-06 14:32:42 +1100

    chore(common): CHECKOUT-6970 Add basic form field component

---
+++ packages/ui/src/form/BasicFormField/BasicFormField.spec.tsx
@@ -0,0 +1,64 @@
+import { mount, shallow } from 'enzyme';
+import { Formik } from 'formik';
+import { noop } from 'lodash';
+import React from 'react';
+
+import FormContext from '../context/FormContext';
+
+import BasicFormField from './BasicFormField';
+
+describe('BasicFormField', () => {
+    it('matches snapshot', () => {
+        expect(shallow(<BasicFormField name="foobar" />)).toMatchSnapshot();
+    });
+
+    it('renders component with test ID', () => {
+        const component = mount(
+            <Formik
+                initialValues={{ foobar: 'foobar' }}
+                onSubmit={noop}
+                render={() => <BasicFormField name="foobar" testId="test" />}
+            />,
+        );
+
+        expect(component.find('.form-field').prop('data-test')).toBe('test');
+    });
+
+    it('changes appearance when there is error', async () => {
+        const component = mount(
+            <FormContext.Provider value={{ isSubmitted: true, setSubmitted: jest.fn() }}>
+                <Formik
+                    initialValues={{ foobar: '' }}
+                    onSubmit={noop}
+                    render={() => (
+                        <BasicFormField
+                            name="foobar"
+                            render={({ field }) => <input {...field} type="text" />}
+                            validate={() => 'Invalid'}
+                        />
+                    )}
+                />
+            </FormContext.Provider>,
+        );
+
+        component.find('input[name="foobar"]').simulate('change').simulate('blur');
+
+        await new Promise((resolve) => process.nextTick(resolve));
+
+        component.update();
+
+        expect(component.find('.form-field').hasClass('form-field--error')).toBe(true);
+    });
+
+    it('renders input component by default', () => {
+        const component = mount(
+            <Formik
+                initialValues={{ foobar: '' }}
+                onSubmit={noop}
+                render={() => <BasicFormField name="foobar" />}
+            />,
+        );
+
+        expect(component.exists('input')).toBe(true);
+    });
+});
