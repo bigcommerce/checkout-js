3230b680e7149251e39508c6af9c4bb46a025858
animesh1987 <animeshsingh1987@gmail.com>
2022-10-10 21:34:58 +1100

    chore(common): CHECKOUT-6970 Add form component

--- packages/ui/src/form/index.ts
+++ packages/ui/src/form/index.ts
@@ -2,3 +2,4 @@ export { default as Input } from './Input/Input';
 export { default as TextArea } from './TextArea/TextArea';
 export { default as TextInput } from './TextInput/TextInput';
 export { default as TextInputIframeContainer } from './TextInputIframeContainer/TextInputIframeContainer';
+export { default as FormContext, FormContextType } from './context/FormContext';
