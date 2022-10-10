fb779d506610bf8100d1cbfd30354eb3332d1705
animesh1987 <animeshsingh1987@gmail.com>
2022-10-10 16:17:24 +1100

    chore(common): CHECKOUT-6970 Add dynamic form field component

--- packages/locale/src/TranslatedString/TranslatedString.tsx
+++ packages/locale/src/TranslatedString/TranslatedString.tsx
@@ -1,15 +1,17 @@
 import { TranslationData } from '@bigcommerce/checkout-sdk';
 import React, { FunctionComponent } from 'react';
 
-import { PaymentMethodProps } from '@bigcommerce/checkout/payment-integration-api';
+import { useLocale } from '../context';
 
-export interface TranslatedStringProps extends Pick<PaymentMethodProps, 'language'> {
-  id: string;
-  data?: TranslationData;
+export interface TranslatedStringProps {
+    id: string;
+    data?: TranslationData;
 }
 
-const TranslatedString: FunctionComponent<TranslatedStringProps> = ({ data, id, language }) => (
-  <>{language.translate(id, data)}</>
-);
+const TranslatedString: FunctionComponent<TranslatedStringProps> = ({ data, id }) => {
+    const { language } = useLocale();
+
+    return <>{language.translate(id, data)}</>;
+};
 
 export default TranslatedString;
