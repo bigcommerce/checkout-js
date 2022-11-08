1805c9609eec2b82d7e47142a9c1d04d8176f636
animesh1987 <animeshsingh1987@gmail.com>
2022-11-08 14:58:36 +1100

    chore(common): CHECKOUT-6440 Add test utils package and signout test

--- jest-setup.ts
+++ jest-setup.ts
@@ -3,32 +3,45 @@ import * as Adapter from 'enzyme-adapter-react-16';
 import { noop } from 'lodash';
 
 const adapter = Adapter as any;
+
 configure({ adapter: new adapter.default() });
 
 // https://github.com/FezVrasta/popper.js/issues/478
 if (window.document) {
-    document.createRange = () => ({
-        setStart: noop,
-        setEnd: noop,
-        commonAncestorContainer: {
-            nodeName: 'BODY',
-            ownerDocument: document,
-        },
-    } as Range);
+    document.createRange = () =>
+        ({
+            setStart: noop,
+            setEnd: noop,
+            commonAncestorContainer: {
+                nodeName: 'BODY',
+                ownerDocument: document,
+            },
+        } as Range);
 }
 
-window.matchMedia = jest.fn(() => ({
-    matches: false,
-    addListener: noop,
-    addEventListener: noop,
-    removeListener: noop,
-    removeEventListener: noop,
-} as MediaQueryList));
+window.matchMedia = jest.fn(
+    () =>
+        ({
+            matches: false,
+            addListener: noop,
+            addEventListener: noop,
+            removeListener: noop,
+            removeEventListener: noop,
+        } as MediaQueryList),
+);
 
-Object.defineProperty(window.navigator, 'userAgent', (value => ({
-    get() { return value; },
-    set(v) { value = v; },
-}))(window.navigator.userAgent));
+Object.defineProperty(
+    window.navigator,
+    'userAgent',
+    ((value) => ({
+        get() {
+            return value;
+        },
+        set(v) {
+            value = v;
+        },
+    }))(window.navigator.userAgent),
+);
 
 (global as any).__webpack_public_path__ = undefined;
 
