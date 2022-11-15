5295e5bea9fb035c00abf33bfefdefa4e806047d
animesh1987 <animeshsingh1987@gmail.com>
2022-11-15 21:45:34 +1100

    chore(common): CHECKOUT-6440 Remove outdir and default export

--- packages/locale/src/contexts/index.ts
+++ packages/locale/src/contexts/index.ts
@@ -1,2 +1,2 @@
-export { default as LocaleContext, LocaleContextType, useLocale } from './LocaleContext';
+export { LocaleContext, LocaleContextType, useLocale } from './LocaleContext';
 export { default as createLocaleContext } from './createLocaleContext';
