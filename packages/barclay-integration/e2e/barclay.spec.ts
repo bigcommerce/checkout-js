1f87e7654820e7ef4f45651da95580177a65e731
Peng Zhou <88361607+bc-peng@users.noreply.github.com>
2023-01-31 14:13:42 +1100

    test(checkout): CHECKOUT-6482 Offsite payment method e2e test (#1173)
    * test(checkout): CHECKOUT-6482 Offsite payment method e2e test
    
    * test(checkout): CHECKOUT-6482 Address feedback I

---
+++ packages/barclay-integration/e2e/barclay.spec.ts
@@ -0,0 +1,21 @@
+import {
+    PaymentStepAsGuestPreset,
+    test,
+} from '@bigcommerce/checkout/payment-integration-test-framework';
+
+test.describe('Offsite payment method', () => {
+    test('Barclay is working', async ({ assertions, checkout }) => {
+        // Testing environment setup
+        await checkout.use(new PaymentStepAsGuestPreset());
+        await checkout.start('Barclay');
+        await checkout.route('**/pay/initialize', `${__dirname}/support/barclay.ejs`);
+
+        // Playwright actions
+        await checkout.goto();
+        await checkout.selectPaymentMethod('credit_card');
+        await checkout.placeOrder();
+
+        // Assertions
+        await assertions.shouldSeeOrderConfirmation();
+    });
+});
