ad27bfb9af838e8430ccc57c4a2fd5007b28e173
Peng <peng.zhou@bigcommerce.com>
2023-02-10 16:39:24 +1100

    feat(checkout): CHECKOUT-7231 Update idents

--- packages/core/src/app/customer/CheckoutButtonContainer.spec.tsx
+++ packages/core/src/app/customer/CheckoutButtonContainer.spec.tsx
@@ -10,37 +10,37 @@ import { createLocaleContext, LocaleContext, LocaleContextType, } from '../local
 import CheckoutButtonContainer from './CheckoutButtonContainer';
 
 describe('CheckoutButtonContainer', () => {
-  let localeContext: LocaleContextType;
-  let checkoutService: CheckoutService;
-  let checkoutState: CheckoutSelectors;
+    let localeContext: LocaleContextType;
+    let checkoutService: CheckoutService;
+    let checkoutState: CheckoutSelectors;
 
-  beforeEach(() => {
-    checkoutService = createCheckoutService();
-    checkoutState = checkoutService.getState();
-    localeContext = createLocaleContext(getStoreConfig());
+    beforeEach(() => {
+        checkoutService = createCheckoutService();
+        checkoutState = checkoutService.getState();
+        localeContext = createLocaleContext(getStoreConfig());
 
-    jest.spyOn(checkoutState.data, 'getConfig').mockReturnValue(
-      merge(getStoreConfig(), {
-        checkoutSettings: {
-          features: { 'CHECKOUT-7222.checkout_settings_styling_section': true },
-          remoteCheckoutProviders: ['amazonpay','applepay', 'braintreevisacheckout'],
-        },
-      }),
-    );
-  });
+        jest.spyOn(checkoutState.data, 'getConfig').mockReturnValue(
+            merge(getStoreConfig(), {
+                checkoutSettings: {
+                    features: { 'CHECKOUT-7222.checkout_settings_styling_section': true },
+                    remoteCheckoutProviders: ['amazonpay','applepay', 'braintreevisacheckout'],
+                },
+            }),
+        );
+    });
 
-  it('matches snapshot', () => {
-    const component = render(
-      <CheckoutProvider checkoutService={checkoutService}>
-        <LocaleContext.Provider value={localeContext}>
-          <CheckoutButtonContainer
-            checkEmbeddedSupport={jest.fn()}
-            onUnhandledError={jest.fn()}
-          />
-        </LocaleContext.Provider>
-      </CheckoutProvider>
-    );
+    it('matches snapshot', () => {
+        const component = render(
+            <CheckoutProvider checkoutService={checkoutService}>
+                <LocaleContext.Provider value={localeContext}>
+                    <CheckoutButtonContainer
+                        checkEmbeddedSupport={jest.fn()}
+                        onUnhandledError={jest.fn()}
+                    />
+                </LocaleContext.Provider>
+            </CheckoutProvider>
+        );
 
-    expect(component).toMatchSnapshot();
-  });
+        expect(component).toMatchSnapshot();
+    });
 });
--- packages/core/src/app/customer/CheckoutButtonContainer.tsx
+++ packages/core/src/app/customer/CheckoutButtonContainer.tsx
@@ -6,45 +6,46 @@ import { TranslatedString } from '../locale';
 import CheckoutButtonListV1 from './CheckoutButtonList';
 
 interface CheckoutButtonsListOnTopProps {
-  checkEmbeddedSupport(methodIds: string[]): void;
-  onUnhandledError(error: Error): void;
+    checkEmbeddedSupport(methodIds: string[]): void;
+
+    onUnhandledError(error: Error): void;
 }
 
 const CheckoutButtonContainer: FunctionComponent<CheckoutButtonsListOnTopProps & CheckoutContextProps> = (
-  {
-    checkEmbeddedSupport,
-    checkoutState,
-    checkoutService,
-    onUnhandledError,
-}) => {
-  const {
-    data: {
-      getConfig,
-    },
-    statuses: {
-      isInitializingCustomer,
-    },
-  } = checkoutState;
-  const config = getConfig();
+    {
+        checkEmbeddedSupport,
+        checkoutState,
+        checkoutService,
+        onUnhandledError,
+    }) => {
+    const {
+        data: {
+            getConfig,
+        },
+        statuses: {
+            isInitializingCustomer,
+        },
+    } = checkoutState;
+    const config = getConfig();
 
-  if (!config) {
-    return null;
-  }
+    if (!config) {
+        return null;
+    }
 
-  return (
-    <div className='wallet-buttons-container'>
-      <CheckoutButtonListV1
-        checkEmbeddedSupport={checkEmbeddedSupport}
-        copywritingStringId='remote.start_with_text'
-        deinitialize={checkoutService.deinitializeCustomer}
-        initialize={checkoutService.initializeCustomer}
-        isInitializing={isInitializingCustomer()}
-        methodIds={config.checkoutSettings.remoteCheckoutProviders}
-        onError={onUnhandledError}
-      />
-      <div className='checkout-separator'><span><TranslatedString id='remote.or_text' /></span></div>
-    </div>
-  )
-}
+    return (
+        <div className='wallet-buttons-container'>
+            <CheckoutButtonListV1
+                checkEmbeddedSupport={checkEmbeddedSupport}
+                copywritingStringId='remote.start_with_text'
+                deinitialize={checkoutService.deinitializeCustomer}
+                initialize={checkoutService.initializeCustomer}
+                isInitializing={isInitializingCustomer()}
+                methodIds={config.checkoutSettings.remoteCheckoutProviders}
+                onError={onUnhandledError}
+            />
+            <div className='checkout-separator'><span><TranslatedString id='remote.or_text' /></span></div>
+        </div>
+    );
+};
 
 export default withCheckout((props) => props)(CheckoutButtonContainer);
