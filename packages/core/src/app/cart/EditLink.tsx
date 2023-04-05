5c0189b6456551653dfa16603071c167c29a0280
Josh Smith <joshsmithcode@gmail.com>
2023-04-05 12:12:44 +1000

    add gtm event to edit cart link

--- packages/core/src/app/cart/EditLink.tsx
+++ packages/core/src/app/cart/EditLink.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent, memo } from 'react';
+import TagManager from 'react-gtm-module'
 
 import { TranslatedString } from '../locale';
 
@@ -11,12 +12,27 @@ const EditLink: FunctionComponent<EditLinkProps> = ({ className, url }) => (
     <a
         className={className || 'cart-header-link'}
         data-test="cart-edit-link"
-        href={url}
         id="cart-edit-link"
-        target="_top"
+        onClick={() => editCartClick(url)}
     >
         <TranslatedString id="cart.edit_cart_action" />
     </a>
 );
+
+const editCartClick = (url: string) => {
+    TagManager.initialize({
+        gtmId: 'GTM-M7GCSJV',
+    })
+
+    const tagManagerArgs = {
+        dataLayer: {
+            event: 'editCartClick',
+        }
+    }
+
+    TagManager.dataLayer(tagManagerArgs)
+
+    window.location.href = url
+}
 
 export default memo(EditLink);
