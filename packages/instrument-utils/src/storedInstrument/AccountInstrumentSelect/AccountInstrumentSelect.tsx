40bafc8bcca07abbd4ce1b5d04c09e6e659f0626
bc-nick <nick.tsybulko@bigcommerce.com>
2023-06-14 09:37:08 +0200

    fix(payment): PAYPAL-2602 added comment

--- packages/instrument-utils/src/storedInstrument/AccountInstrumentSelect/AccountInstrumentSelect.tsx
+++ packages/instrument-utils/src/storedInstrument/AccountInstrumentSelect/AccountInstrumentSelect.tsx
@@ -289,6 +289,9 @@ class AccountInstrumentSelect extends PureComponent<AccountInstrumentSelectProps
         const { selectedInstrumentId } = this.props;
 
         if (prevSelectedInstrumentId !== selectedInstrumentId) {
+            // FIXME: Used setTimeout here because setFieldValue call doesnot set value if called before formik is properly mounted.
+            //        This ensures that update Field value is called after formik has mounted.
+            // See GitHub issue: https://github.com/jaredpalmer/formik/issues/930
             setTimeout(() => this.updateFieldValue(selectedInstrumentId));
         }
     }
