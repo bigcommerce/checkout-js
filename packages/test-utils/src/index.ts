c6eb0bddaa881e58640172db53b602f9922cd26f
Serhii Tkachenko <serhii.tkachenko@bigcommerce.com>
2023-07-05 12:43:11 +0300

    test(other): PAYPAL-000 updated test-utils index file with imports of mock methods

--- packages/test-utils/src/index.ts
+++ packages/test-utils/src/index.ts
@@ -1,15 +1,15 @@
+export { getAddress } from './address.mock';
 export { getCart } from './cart.mock';
-export { getCheckout, getCheckoutPayment } from './checkout.mock';
+export {
+    getCheckout,
+    getCheckoutPayment,
+    getCheckoutWithPayments,
+    getCheckoutWithPromotions,
+} from './checkout.mock';
 export { getStoreConfig } from './config.mock';
 export { getConsignment } from './consignment.mock';
+export { getCoupon, getShippingCoupon } from './coupon.mock';
 export { getGuestCustomer, getCustomer } from './customers.mock';
-export {
-    getMobilePaymentMethod,
-    getPaymentMethod,
-    getPaypalCreditPaymentMethod,
-    getBraintreeAchPaymentMethod,
-} from './payment-methods.mock';
-export { getPaymentFormServiceMock } from './payment-form-service.mock';
 export {
     getAccountInstrument,
     getBankInstrument,
@@ -17,4 +17,20 @@ export {
     getInstruments,
     getAchInstrument,
 } from './instruments.mock';
-export { getDigitalItem, getPhysicalItem, getGiftCertificateItem } from './line-item.mock';
+export {
+    getCustomItem,
+    getDigitalItem,
+    getGiftCertificateItem,
+    getPhysicalItem,
+    getPicklistItem,
+} from './line-item.mock';
+export { getPaymentFormServiceMock } from './payment-form-service.mock';
+export {
+    getMobilePaymentMethod,
+    getPaymentMethod,
+    getPaypalCreditPaymentMethod,
+    getBraintreeAchPaymentMethod,
+} from './payment-methods.mock';
+export { getPromotion } from './promotion.mock';
+export { getShippingAddress } from './shipping-address.mock';
+export { getShippingOption, getShippingOptionPickUpStore } from './shippingMethod.mock';
