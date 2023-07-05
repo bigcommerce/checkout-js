f1fb4d233420c16c8ff7fa6b02a22da31a1c5e72
Serhii Tkachenko <serhii.tkachenko@bigcommerce.com>
2023-07-05 14:09:48 +0300

    feat(payment): PAYPAL-2690 added braintree ach vaulting instrument confirmation feature (#1360)

---
+++ packages/braintree-integration/src/BraintreeAch/components/BraintreeAchPaymentForm.tsx
@@ -0,0 +1,148 @@
+import { PaymentMethod } from '@bigcommerce/checkout-sdk';
+import React, { FunctionComponent, useEffect, useState } from 'react';
+
+import {
+    AccountInstrumentFieldset,
+    StoreInstrumentFieldset,
+} from '@bigcommerce/checkout/instrument-utils';
+import { TranslatedString } from '@bigcommerce/checkout/locale';
+import { useCheckout, usePaymentFormContext } from '@bigcommerce/checkout/payment-integration-api';
+
+import { AccountTypes, OwnershipTypes } from '../constants';
+import useBraintreeAchInstruments from '../hooks/useBraintreeAchInstruments';
+import useBraintreeAchValidation from '../hooks/useBraintreeAchValidation';
+
+import BraintreeAchFormFields from './BraintreeAchFormFields';
+import BraintreeAchMandateText from './BraintreeAchMandateText';
+
+export interface BraintreeAchPaymentFormProps {
+    method: PaymentMethod;
+    updateMandateText: (mandateText: string) => void;
+}
+
+const BraintreeAchPaymentForm: FunctionComponent<BraintreeAchPaymentFormProps> = ({
+    method,
+    updateMandateText,
+}) => {
+    const [isValidForm, setIsValidForm] = useState(false);
+    const { checkoutState } = useCheckout();
+    const { paymentForm } = usePaymentFormContext();
+    const { disableSubmit, getFieldValue, getFormValues, setFieldValue } = paymentForm;
+    const {
+        accountInstruments,
+        currentInstrument,
+        handleSelectInstrument,
+        handleUseNewInstrument,
+        isInstrumentFeatureAvailable,
+        shouldShowInstrumentFieldset,
+        shouldCreateNewInstrument,
+        shouldConfirmInstrument,
+    } = useBraintreeAchInstruments(method);
+    const { validateBraintreeAchForm } = useBraintreeAchValidation();
+
+    const resetFormValues = () => {
+        const { firstName, lastName } = checkoutState.data.getBillingAddress() || {};
+
+        const defaultFormValues = {
+            ownershipType: OwnershipTypes.Personal,
+            accountType: AccountTypes.Savings,
+            accountNumber: '',
+            routingNumber: '',
+            businessName: '',
+            firstName: firstName || '',
+            lastName: lastName || '',
+            shouldSaveInstrument: false,
+            shouldSetAsDefaultInstrument: false,
+            instrumentId: currentInstrument?.bigpayToken || '',
+            orderConsent: false,
+        };
+
+        for (const [key, value] of Object.entries(defaultFormValues)) {
+            setFieldValue(key, value);
+        }
+    };
+
+    useEffect(() => {
+        resetFormValues();
+    }, [currentInstrument?.bigpayToken]);
+
+    const shouldShowForm =
+        !shouldShowInstrumentFieldset || shouldCreateNewInstrument || shouldConfirmInstrument;
+    const shouldShowMandateTextCheckbox = shouldShowForm && isValidForm && !shouldConfirmInstrument;
+
+    useEffect(() => {
+        if (shouldShowForm) {
+            const validate = async () => {
+                const braintreeAchFormValues = getFormValues();
+                const isValid = await validateBraintreeAchForm(braintreeAchFormValues);
+
+                if (!isValid) {
+                    setFieldValue('orderConsent', false);
+                }
+
+                setIsValidForm(isValid);
+            };
+
+            void validate();
+        }
+    }, [getFormValues, setFieldValue, setIsValidForm, shouldShowForm, validateBraintreeAchForm]);
+
+    useEffect(() => {
+        const mandateTextConfirmationCheckboxValue = getFieldValue('orderConsent');
+
+        const inValidForInstrumentConfirmation = shouldConfirmInstrument && !isValidForm;
+        const inValidForDefaultTransactions =
+            shouldShowMandateTextCheckbox && !mandateTextConfirmationCheckboxValue;
+
+        const shouldDisableSubmit = shouldShowForm
+            ? inValidForInstrumentConfirmation || inValidForDefaultTransactions
+            : false;
+
+        disableSubmit(method, shouldDisableSubmit);
+    }, [disableSubmit, getFieldValue, isValidForm, method, shouldShowMandateTextCheckbox]);
+
+    return (
+        <div className="checkout-ach-form" data-test="checkout-ach-form">
+            {shouldShowInstrumentFieldset && (
+                <div className="checkout-ach-form__instrument">
+                    <AccountInstrumentFieldset
+                        instruments={accountInstruments}
+                        onSelectInstrument={handleSelectInstrument}
+                        onUseNewInstrument={handleUseNewInstrument}
+                        selectedInstrument={currentInstrument}
+                    />
+                </div>
+            )}
+
+            {shouldConfirmInstrument && (
+                <p>
+                    <strong>
+                        <TranslatedString id="payment.bank_account_instrument_trusted_shipping_address_title_text" />
+                    </strong>
+
+                    <br />
+
+                    <TranslatedString id="payment.bank_account_instrument_trusted_shipping_address_text" />
+                </p>
+            )}
+
+            {shouldShowForm && <BraintreeAchFormFields />}
+
+            {isInstrumentFeatureAvailable && (
+                <StoreInstrumentFieldset
+                    instrumentId={currentInstrument?.bigpayToken}
+                    isAccountInstrument
+                />
+            )}
+
+            {shouldShowMandateTextCheckbox && (
+                <BraintreeAchMandateText
+                    isInstrumentFeatureAvailable={isInstrumentFeatureAvailable}
+                    updateMandateText={updateMandateText}
+                />
+            )}
+        </div>
+    );
+};
+
+export default BraintreeAchPaymentForm;
---
+++ packages/braintree-integration/src/BraintreeAch/hooks/useBraintreeAchValidation.ts
@@ -0,0 +1,128 @@
+import { useCallback } from 'react';
+import { object, string, StringSchema } from 'yup';
+
+import { useLocale } from '@bigcommerce/checkout/locale';
+import {
+    PaymentFormValues,
+    usePaymentFormContext,
+} from '@bigcommerce/checkout/payment-integration-api';
+
+import {
+    BraintreeAchFieldType,
+    businessBraintreeAchFormFields,
+    OwnershipTypes,
+    personalBraintreeAchFormFields,
+} from '../constants';
+
+const useBraintreeAchValidation = () => {
+    const { paymentForm } = usePaymentFormContext();
+    const { language } = useLocale();
+
+    const getValidationSchema = () => {
+        const ownershipType = paymentForm.getFieldValue('ownershipType');
+        const formFields =
+            ownershipType === OwnershipTypes.Business
+                ? businessBraintreeAchFormFields
+                : personalBraintreeAchFormFields;
+
+        const requiredFieldErrorTranslationIds: { [fieldName: string]: string } = {
+            [BraintreeAchFieldType.FirstName]: 'address.first_name',
+            [BraintreeAchFieldType.LastName]: 'address.last_name',
+            [BraintreeAchFieldType.AccountNumber]: 'payment.errors.account_number',
+            [BraintreeAchFieldType.RoutingNumber]: 'payment.errors.routing_number',
+            [BraintreeAchFieldType.BusinessName]: 'payment.errors.business_name',
+        };
+
+        return object(
+            formFields.reduce((schema, { id, required }) => {
+                if (required) {
+                    if (requiredFieldErrorTranslationIds[id]) {
+                        schema[id] = string().required(
+                            language.translate(
+                                `${requiredFieldErrorTranslationIds[id]}_required_error`,
+                            ),
+                        );
+
+                        if (id === BraintreeAchFieldType.AccountNumber) {
+                            schema[id] = schema[id].matches(
+                                /^\d+$/,
+                                language.translate('payment.errors.only_numbers_error', {
+                                    label: language.translate('payment.account_number_label'),
+                                }),
+                            );
+                        }
+
+                        if (id === BraintreeAchFieldType.RoutingNumber) {
+                            schema[id] = schema[id]
+                                .matches(
+                                    /^\d+$/,
+                                    language.translate('payment.errors.only_numbers_error', {
+                                        label: language.translate('payment.account_routing_label'),
+                                    }),
+                                )
+                                .min(
+                                    8,
+                                    language.translate('customer.min_error', {
+                                        label: language.translate('payment.account_routing_label'),
+                                        min: 8,
+                                    }),
+                                )
+                                .max(
+                                    9,
+                                    language.translate('customer.max_error', {
+                                        label: language.translate('payment.account_routing_label'),
+                                        max: 9,
+                                    }),
+                                );
+                        }
+                    }
+                }
+
+                return schema;
+            }, {} as { [key: string]: StringSchema }),
+        );
+    };
+
+    const validateBraintreeAchForm = useCallback(
+        async (braintreeAchFormValues: PaymentFormValues): Promise<boolean> => {
+            const {
+                accountNumber,
+                businessName,
+                routingNumber,
+                ownershipType,
+                firstName,
+                lastName,
+            } = braintreeAchFormValues;
+
+            const validationSchema = getValidationSchema();
+
+            const [
+                isValidAccountNumber,
+                isValidRoutingNumber,
+                isValidFirstName,
+                isValidLastName,
+                isValidBusinessName,
+            ] = await Promise.all([
+                await validationSchema.fields.accountNumber?.isValid(accountNumber),
+                await validationSchema.fields.routingNumber?.isValid(routingNumber),
+                await validationSchema.fields.firstName?.isValid(firstName),
+                await validationSchema.fields.lastName?.isValid(lastName),
+                await validationSchema.fields.businessName?.isValid(businessName),
+            ]);
+
+            const isValidDepositoryName =
+                ownershipType === OwnershipTypes.Business
+                    ? isValidBusinessName
+                    : isValidFirstName && isValidLastName;
+
+            return isValidRoutingNumber && isValidAccountNumber && isValidDepositoryName;
+        },
+        [getValidationSchema],
+    );
+
+    return {
+        validateBraintreeAchForm,
+    };
+};
+
+export default useBraintreeAchValidation;
