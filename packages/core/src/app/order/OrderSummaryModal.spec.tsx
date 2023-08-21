78d7581064825b6d46e35c29e946ce3bcffcb119
Animesh Singh <animeshsingh1987@gmail.com>
2023-08-21 10:11:15 +1000

    fix(order): CHECKOUT-4355 Fix display issue of pick list items (#1446)

--- packages/core/src/app/order/OrderSummaryModal.spec.tsx
+++ packages/core/src/app/order/OrderSummaryModal.spec.tsx
@@ -1,12 +1,15 @@
-import { Order } from '@bigcommerce/checkout-sdk';
+import { LineItemMap, Order } from '@bigcommerce/checkout-sdk';
 import { mount, shallow, ShallowWrapper } from 'enzyme';
 import React from 'react';
+
+import { getPhysicalItem } from '@bigcommerce/checkout/test-utils';
 
 import { getStoreConfig } from '../config/config.mock';
 import { SMALL_SCREEN_MAX_WIDTH } from '../ui/responsive';
 
 import mapToOrderSummarySubtotalsProps from './mapToOrderSummarySubtotalsProps';
 import { getOrder } from './orders.mock';
+import OrderSummaryItems from './OrderSummaryItems';
 import OrderSummaryModal from './OrderSummaryModal';
 
 let order: Order;
@@ -32,6 +35,37 @@ describe('OrderSummaryModal', () => {
     it('renders order summary', () => {
         expect(orderSummary).toMatchSnapshot();
     });
+
+    describe('it renders order summary modal for bundled items', () => {
+        it('filters bundled items from line items', () => {
+            const lineItems: LineItemMap = {
+                physicalItems: [],
+                digitalItems: [],
+                giftCertificates: [],
+                customItems: [],
+            };
+
+            lineItems.physicalItems.push(getPhysicalItem(), {...getPhysicalItem(), id: '888', parentId: 'test'})
+
+            const order = { ...getOrder(), lineItems };
+
+            orderSummary = shallow(
+                <OrderSummaryModal
+                    isOpen={true}
+                    {...mapToOrderSummarySubtotalsProps(order)}
+                    additionalLineItems="foo"
+                    lineItems={order.lineItems}
+                    shopperCurrency={getStoreConfig().shopperCurrency}
+                    storeCurrency={getStoreConfig().currency}
+                    total={order.orderAmount}
+                />,
+            );
+
+            const itemsComponent = orderSummary.find(OrderSummaryItems);
+            
+            expect(itemsComponent.props().items.physicalItems).toHaveLength(1);
+        });
+    })
 
     describe('when taxes are inclusive', () => {
         it('displays tax as summary section', () => {
