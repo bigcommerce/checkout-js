8443f549a99c12d806a0cc1c5327357c77794f5d
Serhii Tkachenko <serhii.tkachenko@bigcommerce.com>
2023-09-25 10:32:13 +0300

    fix(other): PAYPAL-2891 fixed the issue with webpack watch failed on linked checkout sdk watch recompilation (#1498)

--- webpack.config.js
+++ webpack.config.js
@@ -251,17 +251,23 @@ function loaderConfig(options, argv) {
                 plugins: [
                     new AsyncHookPlugin({
                         onRun({ compiler, done }) {
-                            eventEmitter.on('app:done', () => {
-                                const definePlugin = new DefinePlugin({
-                                    LIBRARY_NAME: JSON.stringify(LIBRARY_NAME),
-                                    MANIFEST_JSON: JSON.stringify(require(
-                                        join(__dirname, isProduction ? 'dist' : 'build', 'manifest.json')
-                                    )),
-                                });
+                            let wasTriggeredBefore = false;
 
-                                definePlugin.apply(compiler);
-                                eventEmitter.emit('loader:done');
-                                done();
+                            eventEmitter.on('app:done', () => {
+                                if (!wasTriggeredBefore) {
+                                    const definePlugin = new DefinePlugin({
+                                        LIBRARY_NAME: JSON.stringify(LIBRARY_NAME),
+                                        MANIFEST_JSON: JSON.stringify(require(
+                                          join(__dirname, isProduction ? 'dist' : 'build', 'manifest.json')
+                                        )),
+                                    });
+
+                                    definePlugin.apply(compiler);
+                                    eventEmitter.emit('loader:done');
+                                    done();
+
+                                    wasTriggeredBefore = true;
+                                }
                             });
 
                             eventEmitter.on('app:error', () => {
