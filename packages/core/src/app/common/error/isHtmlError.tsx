d07b729820ac1a0e6283bf5538247fe240db0940
Andrii <andrii.vitvitskyi@bigcommerce.com>
2023-10-24 12:18:02 +0300

    feat(payment): PAYPAL-2975 added ability to show ratepay specific errors

--- packages/core/src/app/common/error/isHtmlError.tsx
+++ packages/core/src/app/common/error/isHtmlError.tsx
@@ -2,10 +2,10 @@ import { CustomError, isCustomError } from './index';
 
 export default function isHtmlError(error: Error): error is CustomError {
 
-    return isCustomError(error) && (
+    return isCustomError(error) ? (
         error &&
         error.data &&
         typeof error.data.shouldBeTranslatedAsHtml === 'boolean' &&
         typeof error.data.translationKey === 'string'
-    );
+    ): false;
 }
