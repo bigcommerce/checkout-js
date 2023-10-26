4de06b16e907f9dee668d1a3bbc1451391e33944
Kate Scherz <kate.scherz@gladeye.com>
2023-10-26 14:28:09 +1300

    Remove GTM init from code

--- packages/core/src/app/checkout/CheckoutApp.tsx
+++ packages/core/src/app/checkout/CheckoutApp.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable */
-// @ts-nocheck
 import { createCheckoutService, createEmbeddedCheckoutMessenger } from '@bigcommerce/checkout-sdk';
 import { BrowserOptions } from '@sentry/browser';
 import React, { Component } from 'react';
@@ -55,16 +53,6 @@ export default class CheckoutApp extends Component<CheckoutAppProps> {
         const { containerId } = this.props;
 
         ReactModal.setAppElement(`#${containerId}`);
-
-        const initGTM = () => {
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://gtmss.mitoq.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-98LH');
-        }
-
-        setTimeout(initGTM, 5000);
     }
 
     render() {
