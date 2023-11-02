bead26d748edc4624cbde260d596873515282384
Richa Patel <richa.patel@bigcommerce.com>
2023-11-02 22:19:46 +1100

    fix(checkout): CHECKOUT-7422 add comment

--- packages/test-framework/src/fixture/pageObject/playwright/PlaywrightHelper.ts
+++ packages/test-framework/src/fixture/pageObject/playwright/PlaywrightHelper.ts
@@ -40,6 +40,41 @@ export class PlaywrightHelper {
         }
     }
 
+    async mockRoutes(): Promise<void> {
+        await this.page.route('**/internalapi/v1/store/countries', (route) => {
+            const json = {
+                data: [
+                    {
+                        code: 'AU',
+                        name: 'Australia',
+                        subdivisions: [
+                            { code: 'NSW', name: 'New South Wales' },
+                            { code: 'VIC', name: 'Victoria' },
+                        ],
+                        hasPostalCodes: true,
+                        requiresState: true,
+                    },
+                    {
+                        code: 'US',
+                        name: 'United States',
+                        hasPostalCodes: true,
+                        requiresState: true,
+                        subdivisions: [
+                            { code: 'CA', name: 'California' },
+                            { code: 'TX', name: 'Texas' },
+                            { code: 'NY', name: 'New York' },
+                        ],
+                    },
+                ],
+            };
+            void route.fulfill({
+                status: 200,
+                contentType: 'application/json',
+                json,
+            });
+        });
+    }
+
     async useHAR(har: string, folder: string): Promise<void> {
         this.har = har;
 
@@ -53,40 +88,10 @@ export class PlaywrightHelper {
         await this.page.route(/.*\/products\/.*\/images\/.*/, (route) => {
             void route.fulfill({ status: 200, path: `${this.srcPath}/support/product.png` });
         });
-        if (this.isReplay) {
-            await this.page.route('**/internalapi/v1/store/countries', (route) => {
-                const json = {
-                    data: [
-                        {
-                            code: 'AU',
-                            name: 'Australia',
-                            subdivisions: [
-                                { code: 'NSW', name: 'New South Wales' },
-                                { code: 'VIC', name: 'Victoria' },
-                            ],
-                            hasPostalCodes: true,
-                            requiresState: true,
-                        },
-                        {
-                            code: 'US',
-                            name: 'United States',
-                            hasPostalCodes: true,
-                            requiresState: true,
-                            subdivisions: [
-                                { code: 'CA', name: 'California' },
-                                { code: 'TX', name: 'Texas' },
-                                { code: 'NY', name: 'New York' },
-                            ],
-                        },
-                    ],
-                };
-                void route.fulfill({
-                    status: 200,
-                    contentType: 'application/json',
-                    json,
-                });
-            });
 
+        // TODO: Remove these mocks when updating HAR files
+        await this.mockRoutes();
+        if (this.isReplay) {
             // creating local checkout environment during replay
             this.polly.enableReplay();
 
