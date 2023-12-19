3ccab3aba8203b060d3b70db8e3509b8070747d4
bc-wpietrzak <weronika.pietrzak@bigcommerce.com>
2023-12-19 09:31:04 +0100

    fix(payment): PI-1163 fix error when paying with default stored card

--- packages/squarev2-integration/src/SquareV2Form.tsx
+++ packages/squarev2-integration/src/SquareV2Form.tsx
@@ -136,7 +136,7 @@ const SquareV2Form: FunctionComponent<SquareV2FormProps> = ({
                         onDeleteInstrument={handleDeleteInstrument}
                         onSelectInstrument={handleSelectInstrument}
                         onUseNewInstrument={handleUseNewCard}
-                        selectedInstrumentId={selectedInstrumentId}
+                        selectedInstrumentId={getDefaultInstrumentId()}
                     />
                 )}
                 <div
