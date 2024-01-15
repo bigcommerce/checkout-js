440d6aaea7896788a736b47803f912fb31e7086b
Serhii Tkachenko <serhii.tkachenko@bigcommerce.com>
2024-01-15 17:23:16 +0200

    feat(payment): PAYPAL-3405 added PayPalCommerceAcceleratedCheckout payment method component

--- packages/paypal-commerce-integration/src/index.ts
+++ packages/paypal-commerce-integration/src/index.ts
@@ -1,4 +1,5 @@
 export { default as PayPalCommerceAPMsPaymentMethod } from './PayPalCommerceAPMsPaymentMethod';
+export { default as PayPalCommerceAcceleratedCheckoutPaymentMethod } from './PayPalCommerceAcceleratedCheckout';
 export { default as PayPalCommerceCreditPaymentMethod } from './PayPalCommerceCreditPaymentMethod';
 export { default as PayPalCommercePaymentMethod } from './PayPalCommercePaymentMethod';
 export { default as PayPalCommerceVenmoPaymentMethod } from './PayPalCommerceVenmoPaymentMethod';
