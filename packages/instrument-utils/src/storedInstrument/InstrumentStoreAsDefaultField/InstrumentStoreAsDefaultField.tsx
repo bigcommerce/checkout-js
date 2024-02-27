4120db59ab8734d4caa459648450a073a2e969aa
Andrii <andrii.vitvitskyi@bigcommerce.com>
2024-02-27 11:57:49 +0200

    fix(payment): PAYPAL-3566 fixed vaulting checkboxes

--- packages/instrument-utils/src/storedInstrument/InstrumentStoreAsDefaultField/InstrumentStoreAsDefaultField.tsx
+++ packages/instrument-utils/src/storedInstrument/InstrumentStoreAsDefaultField/InstrumentStoreAsDefaultField.tsx
@@ -1,7 +1,8 @@
-import React, { FunctionComponent, memo, useMemo } from 'react';
+import React, { FunctionComponent, memo, useEffect, useMemo } from 'react';
 
 import { TranslatedString } from '@bigcommerce/checkout/locale';
 import { CheckboxFormField } from '@bigcommerce/checkout/ui';
+import { usePaymentFormContext } from '@bigcommerce/checkout/payment-integration-api';
 
 interface InstrumentStoreAsDefaultFieldProps {
     isAccountInstrument: boolean;
@@ -12,9 +13,16 @@ const InstrumentStoreAsDefaultField: FunctionComponent<InstrumentStoreAsDefaultF
     isAccountInstrument,
     disabled = false,
 }) => {
+    const { paymentForm } = usePaymentFormContext();
     const translationId = isAccountInstrument
         ? 'payment.account_instrument_save_as_default_payment_method_label'
         : 'payment.instrument_save_as_default_payment_method_label';
+
+    useEffect(() => {
+        if (disabled) {
+            paymentForm.setFieldValue('shouldSetAsDefaultInstrument', false);
+        }
+    }, [disabled, paymentForm]);
 
     const labelContent = useMemo(() => <TranslatedString id={translationId} />, [translationId]);
 
