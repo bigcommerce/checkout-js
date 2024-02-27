080da497859fe14e94859fe32d5f5961b0ade83e
Sebastian Szafraniec <sebastian.szafraniec@bigcommerce.com>
2024-02-27 15:22:34 +0100

    feat(payment): PI-1625 [Affirm][CheckoutJS] Refactor the existing `Affirm` components in Checkout JS to use the new checkout payment integration JS API

---
+++ packages/affirm-integration/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    displayName: 'affirm-integration',
+    preset: '../../jest.preset.js',
+    globals: {
+        'ts-jest': {
+            tsconfig: '<rootDir>/tsconfig.spec.json',
+            diagnostics: false,
+        },
+    },
+    setupFilesAfterEnv: ['../../jest-setup.ts'],
+    coverageDirectory: '../../coverage/packages/affirm-integration',
+};
