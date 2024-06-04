9cddafd2d2ce7260e1e62537fade833e87e46004
bc-nick <nick.tsybulko@bigcommerce.com>
2024-06-04 12:00:47 +0200

    feat(payment): PAYPAL-3520 fix tests

--- packages/braintree-integration/src/BraintreePaypalPaymentMethod.test.tsx
+++ packages/braintree-integration/src/BraintreePaypalPaymentMethod.test.tsx
@@ -1,26 +1,29 @@
 import { createCheckoutService, LanguageService } from '@bigcommerce/checkout-sdk';
-import { createLocaleContext, LocaleContext, LocaleContextType } from '@bigcommerce/checkout/locale';
-import { mount } from 'enzyme';
 import { EventEmitter } from 'events';
 import { Formik } from 'formik';
 import { noop } from 'lodash';
 import React, { FunctionComponent } from 'react';
 
 import {
+    createLocaleContext,
+    LocaleContext,
+    LocaleContextType,
+} from '@bigcommerce/checkout/locale';
+import {
     CheckoutProvider,
     PaymentFormContext,
     PaymentFormService,
 } from '@bigcommerce/checkout/payment-integration-api';
 import {
-    getBraintreePaypalPaymentMethod, getCart, getCustomer,
+    getBraintreePaypalPaymentMethod,
+    getCart,
+    getCustomer,
     getPaymentFormServiceMock,
-    getStoreConfig
+    getStoreConfig,
 } from '@bigcommerce/checkout/test-mocks';
 import { render } from '@bigcommerce/checkout/test-utils';
 
-import HostedPaymentMethodComponent from 'packages/hosted-payment-integration/src/HostedPaymentComponent';
-
-import { BraintreePaypalPaymentMethod } from './index';
+import BraintreePaypalPaymentMethod from './BraintreePaypalPaymentMethod';
 
 describe('BraintreePaypalPaymentMethod', () => {
     let eventEmitter: EventEmitter;
@@ -38,10 +41,16 @@ describe('BraintreePaypalPaymentMethod', () => {
         method: getBraintreePaypalPaymentMethod(),
         onUnhandledError: jest.fn(),
 
-        paymentForm: jest.fn() as unknown as PaymentFormService,
+        paymentForm: {
+            hidePaymentSubmitButton: jest.fn(),
+            setSubmitted: jest.fn(),
+            submitForm: jest.fn(),
+        } as unknown as PaymentFormService,
     };
 
     beforeEach(() => {
+        const providerError = new Error('INSTRUMENT_DECLINED');
+
         eventEmitter = new EventEmitter();
         paymentForm = getPaymentFormServiceMock();
         localeContext = createLocaleContext(getStoreConfig());
@@ -51,6 +60,29 @@ describe('BraintreePaypalPaymentMethod', () => {
         jest.spyOn(checkoutState.data, 'getConfig').mockReturnValue(getStoreConfig());
 
         jest.spyOn(checkoutState.data, 'getCustomer').mockReturnValue(getCustomer());
+
+        jest.spyOn(checkoutService, 'initializePayment').mockImplementation((options) => {
+            eventEmitter.on('onRenderButton', () => {
+                if (options.braintree?.onRenderButton) {
+                    // eslint-disable-next-line
+                    options.braintree.onRenderButton();
+                }
+            });
+
+            eventEmitter.on('onError', () => {
+                if (options.braintree?.onError) {
+                    options.braintree.onError(providerError);
+                }
+            });
+
+            eventEmitter.on('submitForm', () => {
+                if (options.braintree?.submitForm) {
+                    options.braintree.submitForm();
+                }
+            });
+
+            return Promise.resolve(checkoutState);
+        });
 
         BraintreePaypalPaymentMethodTest = (props) => (
             <CheckoutProvider checkoutService={checkoutService}>
@@ -63,12 +95,6 @@ describe('BraintreePaypalPaymentMethod', () => {
                 </PaymentFormContext.Provider>
             </CheckoutProvider>
         );
-    });
-
-    it('renders as hosted payment method', () => {
-        const container = mount(<BraintreePaypalPaymentMethodTest />);
-
-        expect(container.find(HostedPaymentMethodComponent)).toHaveLength(1);
     });
 
     it('initializes BraintreePaypalPaymentMethod', () => {
@@ -85,30 +111,48 @@ describe('BraintreePaypalPaymentMethod', () => {
                 onError: expect.any(Function),
                 onRenderButton: expect.any(Function),
                 submitForm: expect.any(Function),
-                containerId: "#checkout-payment-continue",
-            }
+                containerId: '#checkout-payment-continue',
+            },
         });
     });
 
-    it('throws specific error text when receive INSTRUMENT_DECLINED error message', async () => {
-        const providerError = new Error('INSTRUMENT_DECLINED');
-
-        jest.spyOn(checkoutService, 'initializePayment').mockImplementation((options) => {
-            eventEmitter.on('onError', () => {
-                if (options.braintree?.onError) {
-                    options.braintree.onError(providerError);
-                }
-            });
-
-            return Promise.resolve(checkoutState);
-        });
-
-        mount(<BraintreePaypalPaymentMethodTest />);
+    it('throws specific error text when receive INSTRUMENT_DECLINED error message', () => {
+        render(<BraintreePaypalPaymentMethodTest />);
 
         eventEmitter.emit('onError');
 
         expect(defaultProps.onUnhandledError).toHaveBeenCalledWith(
             new Error(defaultProps.language.translate('payment.errors.instrument_declined')),
         );
+    });
+
+    it('hides payment submit button by calling onRenderButton callback', () => {
+        jest.spyOn(checkoutService, 'initializePayment').mockImplementation((options) => {
+            eventEmitter.on('onRenderButton', () => {
+                if (options.braintree?.onRenderButton) {
+                    options.braintree.onRenderButton();
+                }
+            });
+
+            return Promise.resolve(checkoutState);
+        });
+
+        render(<BraintreePaypalPaymentMethodTest />);
+
+        eventEmitter.emit('onRenderButton');
+
+        expect(defaultProps.paymentForm.hidePaymentSubmitButton).toHaveBeenCalledWith(
+            defaultProps.method,
+            true,
+        );
+    });
+
+    it('submits payment form by calling submitForm callback', () => {
+        render(<BraintreePaypalPaymentMethodTest />);
+
+        eventEmitter.emit('submitForm');
+
+        expect(defaultProps.paymentForm.setSubmitted).toHaveBeenCalledWith(true);
+        expect(defaultProps.paymentForm.submitForm).toHaveBeenCalled();
     });
 });
