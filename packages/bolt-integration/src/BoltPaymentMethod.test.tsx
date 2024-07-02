0cf188ce8705a234fce00c0fae34836f1d333455
Weronika Pietrzak <138816572+bc-wpietrzak@users.noreply.github.com>
2024-07-02 22:47:16 +0200

    feat(payment): PI-1535 move `bolt` components to a separate package (#1898)

---
+++ packages/bolt-integration/src/BoltPaymentMethod.test.tsx
@@ -0,0 +1,71 @@
+import {
+    CheckoutSelectors,
+    CheckoutService,
+    createCheckoutService,
+    createLanguageService,
+} from '@bigcommerce/checkout-sdk';
+import { render } from '@testing-library/react';
+import React, { FunctionComponent } from 'react';
+
+import { PaymentMethodId, PaymentMethodProps } from '@bigcommerce/checkout/payment-integration-api';
+import {
+    getCart,
+    getCustomer,
+    getPaymentFormServiceMock,
+    getPaymentMethod,
+    getStoreConfig,
+} from '@bigcommerce/checkout/test-mocks';
+
+import BoltPaymentMethod from './BoltPaymentMethod';
+
+describe('when using Bolt payment', () => {
+    let checkoutService: CheckoutService;
+    let checkoutState: CheckoutSelectors;
+    let defaultProps: PaymentMethodProps;
+    let PaymentMethodTest: FunctionComponent;
+
+    beforeEach(() => {
+        checkoutService = createCheckoutService();
+        checkoutState = checkoutService.getState();
+
+        defaultProps = {
+            checkoutService,
+            checkoutState,
+            language: createLanguageService(),
+            method: {
+                ...getPaymentMethod(),
+                id: PaymentMethodId.Bolt,
+                initializationData: {
+                    embeddedOneClickEnabled: false,
+                },
+            },
+            onUnhandledError: jest.fn(),
+            paymentForm: getPaymentFormServiceMock(),
+        };
+
+        jest.spyOn(checkoutState.data, 'getCart').mockReturnValue(getCart());
+
+        jest.spyOn(checkoutState.data, 'getConfig').mockReturnValue(getStoreConfig());
+
+        jest.spyOn(checkoutState.data, 'getCustomer').mockReturnValue(getCustomer());
+
+        PaymentMethodTest = (props) => <BoltPaymentMethod {...defaultProps} {...props} />;
+    });
+
+    it('renders as bolt client payment method if embeddedOneClickEnabled is false', () => {
+        defaultProps.method.initializationData.embeddedOneClickEnabled = false;
+
+        const { container } = render(<PaymentMethodTest />);
+
+        expect(container.getElementsByClassName('form-field--bolt-embed')).toHaveLength(0);
+    });
+
+    it('renders as bolt embedded payment method if embeddedOneClickEnabled is true', () => {
+        defaultProps.method.initializationData.embeddedOneClickEnabled = true;
+
+        const { container } = render(<PaymentMethodTest />);
+
+        expect(container.getElementsByClassName('payment-widget')).toHaveLength(1);
+        expect(container.getElementsByClassName('form-field--bolt-embed')).toHaveLength(1);
+    });
+});
---
+++ packages/bolt-integration/src/index.ts
@@ -0,0 +1,3 @@
+export { default as BoltClientPaymentMethod } from './BoltClientPaymentMethod';
+export { default as BoltEmbeddedPaymentMethod } from './BoltEmbeddedPaymentMethod';
+export { default as BoltPaymentMethod } from './BoltPaymentMethod';
