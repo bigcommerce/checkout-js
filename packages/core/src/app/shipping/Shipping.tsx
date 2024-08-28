3299f41d63e1c7281ea687a9f6c513f46fa4a469
David Chin <davidchin.yf@gmail.com>
2024-08-28 15:07:39 +1000

    fix(checkout): CHECKOUT-8300 Add experiment to gate the change

--- packages/core/src/app/shipping/Shipping.tsx
+++ packages/core/src/app/shipping/Shipping.tsx
@@ -149,7 +149,7 @@ class Shipping extends Component<ShippingProps & WithCheckoutShippingProps, Ship
                 initialize={initializeShippingMethod}
                 isBillingSameAsShipping={isBillingSameAsShipping}
                 isGuest={ isGuest }
-                isInitialValueLoaded={!isInitializing}
+                isInitialValueLoaded={shouldRenderWhileLoading ? !isInitializing : true}
                 isLoading={ isInitializing }
                 isMultiShippingMode={isMultiShippingMode}
                 isShippingMethodLoading={ this.props.isLoading }
@@ -178,7 +178,7 @@ class Shipping extends Component<ShippingProps & WithCheckoutShippingProps, Ship
                         isBillingSameAsShipping={isBillingSameAsShipping}
                         isFloatingLabelEnabled={isFloatingLabelEnabled}
                         isGuest={isGuest}
-                        isInitialValueLoaded={!isInitializing}
+                        isInitialValueLoaded={shouldRenderWhileLoading ? !isInitializing : true}
                         isMultiShippingMode={isMultiShippingMode}
                         onMultiShippingSubmit={this.handleMultiShippingSubmit}
                         onSingleShippingSubmit={this.handleSingleShippingSubmit}
