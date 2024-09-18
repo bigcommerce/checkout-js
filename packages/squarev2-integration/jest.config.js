c250c64cb0ff9ec7a3436d721013f0299b985592
Richa Patel <richa.patel@bigcommerce.com>
2024-09-18 19:01:58 +1000

    chore(checkout): CHECKOUT-8562 fix lint

--- packages/squarev2-integration/jest.config.js
+++ packages/squarev2-integration/jest.config.js
@@ -2,10 +2,13 @@ module.exports = {
     displayName: 'squarev2-integration',
     preset: '../../jest.preset.js',
     transform: {
-        '^.+\\.[tj]sx?$': ['ts-jest', {
-            tsconfig: '<rootDir>/tsconfig.spec.json',
-            diagnostics: false,
-        }],
+        '^.+\\.[tj]sx?$': [
+            'ts-jest',
+            {
+                tsconfig: '<rootDir>/tsconfig.spec.json',
+                diagnostics: false,
+            },
+        ],
     },
     setupFilesAfterEnv: ['../../jest-setup.ts'],
     coverageDirectory: '../../coverage/packages/squarev2-integration',
