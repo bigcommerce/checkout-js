6ce796404b600f7c7febb4c71e711f7798ef1cf6
Richa Patel <richa.patel@bigcommerce.com>
2024-10-24 16:47:32 +1100

    feat(shipping): CHECKOUT-8602 add validation for allocate items modal

--- packages/core/src/app/shipping/LeftToAllocateItemsTable.tsx
+++ packages/core/src/app/shipping/LeftToAllocateItemsTable.tsx
@@ -1,39 +1,16 @@
+import { FormikErrors } from "formik";
 import React, { FunctionComponent } from "react";
 
-import { FormField, TextInput } from "../ui/form";
-
+import { AllocateItemsModalFormValues } from "./AllocateItemsModal";
+import LeftToAllocateItem from "./LeftToAllocateItem";
 import { MultiShippingTableItemWithType } from "./MultishippingV2Type";
 
 interface LeftToAllocateItemsTableProps {
     items: MultiShippingTableItemWithType[];
+    formErrors: FormikErrors<AllocateItemsModalFormValues>;
 }
 
-const LeftToAllocateTableItem = ({ item }: { item: MultiShippingTableItemWithType }) => {
-    return (
-        <tr>
-            <td className="left-to-allocate-item-name-container">
-                <figure className="left-to-allocate-item-figure">
-                    {item.imageUrl && <img alt={item.name} src={item.imageUrl} />}
-                </figure>
-                <div className="left-to-allocate-item-name">
-                    <p>{item.name}</p>
-                    {item.options?.map(option => (
-                        <p className="left-to-allocate-item-option" key={option.nameId}>{option.name}: {option.value}</p>
-                    ))}
-                </div>
-            </td>
-            <td>{item.quantity}</td>
-            <td>
-                <FormField
-                    input={({ field }) => <TextInput {...field} disabled={item.quantity === 0} id={field.name} type="number" />}
-                    name={item.id.toString()}
-                />
-            </td>
-        </tr>
-    );
-}
-
-const LeftToAllocateItemsTable: FunctionComponent<LeftToAllocateItemsTableProps> = ({ items }: LeftToAllocateItemsTableProps) => {
+const LeftToAllocateItemsTable: FunctionComponent<LeftToAllocateItemsTableProps> = ({ items, formErrors }: LeftToAllocateItemsTableProps) => {
     return (
         <table className="table left-to-allocate-items-table">
             <thead>
@@ -45,7 +22,11 @@ const LeftToAllocateItemsTable: FunctionComponent<LeftToAllocateItemsTableProps>
             </thead>
             <tbody>
                 {items.map(item => (
-                    <LeftToAllocateTableItem item={item} key={item.id} />      
+                    <LeftToAllocateItem
+                        error={formErrors[item.id.toString()]}
+                        item={item}
+                        key={item.id}
+                    />      
                 ))}
             </tbody>
         </table>
