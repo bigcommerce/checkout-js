02e25efa6a2e64c79ec4bc918e9e721a5d326c0c
Peng Zhou <peng.zhou@bigcommerce.com>
2024-11-29 10:38:18 +1100

    feat(checkout): CHECKOUT-8869 Remove Available Shipping Option Check
    feat(checkout): CHECKOUT-8869 Remove Available Shipping Option Check

--- packages/core/src/app/shipping/hasSelectedShippingOptions.ts
+++ packages/core/src/app/shipping/hasSelectedShippingOptions.ts
@@ -9,13 +9,7 @@ export default function hasSelectedShippingOptions(consignments: Consignment[]):
     return every(
         consignments,
         (consignment) =>
-            (consignment.selectedShippingOption &&
-                consignment.selectedShippingOption.id &&
-                // Selected option is available
-                consignment.availableShippingOptions &&
-                consignment.availableShippingOptions.filter(
-                    ({ id }) => id === consignment.selectedShippingOption?.id,
-                ).length) ||
+            (consignment.selectedShippingOption && consignment.selectedShippingOption.id) ||
             consignment.selectedShippingOption?.type === 'custom',
     );
 }
