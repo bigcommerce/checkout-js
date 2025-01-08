bbed00c86c961aea34bedec0f4b03493ad53176e
Serhii Tkachenko <serhii.tkachenko@bigcommerce.com>
2025-01-08 19:20:01 +0200

    fix(payment): PAYPAL-5026 updated import source for isPayPalCommerceFastlaneMethod method

--- packages/paypal-fastlane-integration/src/index.ts
+++ packages/paypal-fastlane-integration/src/index.ts
@@ -4,7 +4,7 @@ export { default as isPayPalFastlaneAddress } from './is-paypal-fastlane-address
 export { default as isPayPalFastlaneCustomer } from './is-paypal-fastlane-customer';
 export { default as isPayPalFastlaneMethod } from './is-paypal-fastlane-method';
 export { default as isBraintreeFastlaneMethod } from './is-braintree-fastlane-method';
-export { default as isPayPalCommerceFastlaneMethod } from './is-paypal-fastlane-method';
+export { default as isPayPalCommerceFastlaneMethod } from './is-paypal-commerce-fastlane-method';
 export { default as PoweredByPayPalFastlaneLabel } from './PoweredByPayPalFastlaneLabel';
 export { default as PayPalFastlaneShippingAddressForm } from './PayPalFastlaneShippingAddressForm';
 export { default as PayPalFastlaneWatermark } from './PayPalFastlaneWatermark';
