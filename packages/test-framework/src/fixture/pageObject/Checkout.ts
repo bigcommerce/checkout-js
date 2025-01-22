83a2c7b4bab94a75011faf64f3b8391d4b186163
Peng Zhou <peng.zhou@bigcommerce.com>
2025-01-22 15:48:17 +1100

    feat(checkout): CHECKOUT-8959 Fix e2e tests

--- packages/test-framework/src/fixture/pageObject/Checkout.ts
+++ packages/test-framework/src/fixture/pageObject/Checkout.ts
@@ -170,10 +170,6 @@ export class Checkout {
             addressAppendText,
         });
 
-        if (!isBillingAddressSame) await this.page.locator('label[for="sameAsBilling"]').click();
-        if (!shouldSaveAddress)
-            await this.page.locator('label[for="shippingAddress.shouldSaveAddress"]').click();
-
         await this.page.locator('#checkout-shipping-options').waitFor({ state: 'visible' });
 
         const firstShippingMethod = this.page
@@ -181,6 +177,15 @@ export class Checkout {
             .nth(0);
 
         await firstShippingMethod.click();
+
+        if (!shouldSaveAddress) {
+            await this.page.getByText('Save this address in my address book.').click();
+        }
+
+        if (!isBillingAddressSame) {
+            await this.page.locator('label[for="sameAsBilling"]').click();
+        }
+
         await this.page.locator('#checkout-shipping-continue').click();
     }
 
