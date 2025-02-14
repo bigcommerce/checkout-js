0208d276cc58612d6114ba065fe4416b4d8fde28
Giancarlo Byrne <gbyrne@razoyo.com>
2025-02-14 13:01:06 -0700

    Add EZcheck link in order comments

--- packages/core/src/app/order/appendFFLtoCheckoutNotes.ts
+++ packages/core/src/app/order/appendFFLtoCheckoutNotes.ts
@@ -1,24 +1,37 @@
 // @ts-nocheck
-export default async function appendFFLtoCheckoutNotes(checkout, updateCheckout, selectedFFL): Promise<CheckoutSelectors> {
-    // FFL-290 sample customer message to be appended to the checkout
-    // <existing customer message>|FFL#6-04-013-07-5A-03791|Expiration:10/12/2024
-    const months = {
-        "A": "01",
-        "B": "02",
-        "C": "03",
-        "D": "04",
-        "E": "05",
-        "F": "06",
-        "G": "07",
-        "H": "08",
-        "J": "09",
-        "K": "10",
-        "L": "11",
-        "M": "12"
-    }
-    const expiryMonth = months[selectedFFL.slice(13,14)];
-    const expiryYear = `202${selectedFFL.slice(12,13)}`
-    const message = `${checkout.customerMessage}|FFL#${selectedFFL}|Expiration:${expiryMonth}/01/${expiryYear}`
+export default async function appendFFLtoCheckoutNotes(
+  checkout,
+  updateCheckout,
+  selectedFFL,
+): Promise<CheckoutSelectors> {
+  // Appends FFL information to the checkout order comments in the following format:
+  // Format: <existing message>|FFL#<license>|Expiration:<date>|EZcheck:<url>
+  const months = {
+    A: '01',
+    B: '02',
+    C: '03',
+    D: '04',
+    E: '05',
+    F: '06',
+    G: '07',
+    H: '08',
+    J: '09',
+    K: '10',
+    L: '11',
+    M: '12',
+  };
+  const expiryMonth = months[selectedFFL.slice(13, 14)];
+  const expiryYear = `202${selectedFFL.slice(12, 13)}`;
 
-    await updateCheckout({ customerMessage: message });
+  // Parse FFL number components for ATF link
+  // Split by dashes and get relevant parts
+  const fflParts = selectedFFL.split('-');
+  const licsRegn = fflParts[0]; // First part (6)
+  const licsDis = fflParts[1]; // Second part (04)
+  const licsSeq = fflParts[5]; // Last part (03791)
+  const atfLink = `https://fflezcheck.atf.gov/FFLEzCheck/fflSearch?licsRegn=${licsRegn}&licsDis=${licsDis}&licsSeq=${licsSeq}`;
+
+  const message = `${checkout.customerMessage}|FFL#${selectedFFL}|Expiration:${expiryMonth}/01/${expiryYear}|EZcheck:${atfLink}`;
+
+  await updateCheckout({ customerMessage: message });
 }
