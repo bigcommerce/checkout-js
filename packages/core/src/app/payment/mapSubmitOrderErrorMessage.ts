ed4a38f3338542d7ad649629f13b3a79eed9b32d
animesh1987 <animeshsingh1987@gmail.com>
2025-03-06 13:02:43 +1100

    chore(checkout): CHECKOUT-9052 Display correct error heading

--- packages/core/src/app/payment/mapSubmitOrderErrorMessage.ts
+++ packages/core/src/app/payment/mapSubmitOrderErrorMessage.ts
@@ -71,5 +71,9 @@ export function mapSubmitOrderErrorTitle(
         return translate('common.unavailable_heading');
     }
 
+    if (error.type === 'missing_shipping_method') {
+        return translate('common.missing_shipping_method_heading');
+    }
+
     return translate('common.error_heading');
 }
