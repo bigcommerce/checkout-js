da02990fbd7a99827c6928b07eff41318ae75dbd
Richa Patel <richa.patel@bigcommerce.com>
2025-04-10 11:04:10 +1000

    test(checkout): CHECKOUT-9131 remove container

--- packages/core/src/app/order/OrderConfirmationApp.test.tsx
+++ packages/core/src/app/order/OrderConfirmationApp.test.tsx
@@ -10,25 +10,18 @@ jest.mock('./OrderConfirmation', () => (props: any) => (
 
 describe('OrderConfirmationApp', () => {
     let defaultProps: OrderConfirmationAppProps;
-    let container: HTMLElement;
 
     beforeEach(() => {
         defaultProps = {
             orderId: 100,
             containerId: 'bar',
         };
-
-        container = document.createElement('div');
-        container.id = defaultProps.containerId;
-        document.body.appendChild(container);
-    });
-
-    afterEach(() => {
-        document.body.removeChild(container);
     });
 
     it('renders app without crashing', async () => {
-        const { container } = render(<OrderConfirmationApp {...defaultProps} />);
+        const { container } = render(<div id={defaultProps.containerId}>
+            <OrderConfirmationApp {...defaultProps} />
+        </div>);
 
         // eslint-disable-next-line testing-library/no-node-access, testing-library/no-container
         expect(container.querySelector('.orderConfirmationApp')).toBeInTheDocument();
