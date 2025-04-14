d2021e68b55bfd5ff2ace1a047cbfb07e5b60dde
animesh1987 <animeshsingh1987@gmail.com>
2025-04-14 12:03:01 +1000

    chore(other): CHECKOUT-000 Fix reexport warnings

--- packages/locale/src/index.ts
+++ packages/locale/src/index.ts
@@ -1,20 +1,27 @@
-export { default as LocaleContext, LocaleContextType, useLocale } from './LocaleContext';
+export { default as LocaleContext, useLocale } from './LocaleContext';
 export { default as createLocaleContext } from './createLocaleContext';
 export { default as getDefaultTranslations } from './getDefaultTranslations';
-export {
-    default as getLanguageService,
-    InitializeLanguageService,
-    initializeLanguageService,
-} from './getLanguageService';
+export { default as getLanguageService, initializeLanguageService } from './getLanguageService';
 export { default as masterpassFormatLocale } from './masterpassFormatLocale';
-export { default as withCurrency, WithCurrencyProps } from './withCurrency';
-export { default as withLanguage, WithLanguageProps } from './withLanguage';
-export { default as withDate, WithDateProps } from './withDate';
-export { default as LanguageWindow, isLanguageWindow } from './LanguageWindow';
+export { default as withCurrency } from './withCurrency';
+export { default as withLanguage } from './withLanguage';
+export { default as withDate } from './withDate';
+export { isLanguageWindow } from './LanguageWindow';
 export { default as LocaleProvider } from './LocaleProvider';
-export { default as TranslatedHtml, TranslatedHtmlProps } from './TranslatedHtml';
-export { default as TranslatedLink, TranslatedLinkProps } from './TranslatedLink';
-export { default as TranslatedString, TranslatedStringProps } from './TranslatedString';
+export { default as TranslatedHtml } from './TranslatedHtml';
+export { default as TranslatedLink } from './TranslatedLink';
+export { default as TranslatedString } from './TranslatedString';
 export { default as localizeAddress } from './localizeAddress';
 export { getLocaleContext } from './localeContext.mock';
 export { FALLBACK_TRANSLATIONS } from './translations';
+
+// export types separately
+export type { LocaleContextType } from './LocaleContext';
+export type { InitializeLanguageService } from './getLanguageService';
+export type { WithCurrencyProps } from './withCurrency';
+export type { WithLanguageProps } from './withLanguage';
+export type { WithDateProps } from './withDate';
+export type { default as LanguageWindow } from './LanguageWindow';
+export type { default as TranslatedLinkProps } from './TranslatedLink';
+export type { default as TranslatedHtmlProps } from './TranslatedHtml';
+export type { default as TranslatedStringProps } from './TranslatedString';
