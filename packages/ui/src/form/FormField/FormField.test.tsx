ba0c631a34f420c2435dcf9dbb729c9324deeece
animesh1987 <animeshsingh1987@gmail.com>
2025-04-15 21:46:10 +1000

    chore(checkout): CHECKOUT-9144 Add tests in form folder

---
+++ packages/ui/src/form/FormField/FormField.test.tsx
@@ -0,0 +1,50 @@
+import { Formik } from 'formik';
+import React from 'react';
+
+import { render, screen } from '@bigcommerce/checkout/test-utils';
+
+import FormField, { FormFieldProps } from './FormField';
+
+describe('FormField', () => {
+    let defaultProps: FormFieldProps;
+
+    beforeEach(() => {
+        defaultProps = {
+            additionalClassName: 'test',
+            labelContent: null,
+            label: null,
+            onChange: jest.fn(),
+            footer: null,
+            input: jest.fn(),
+            name: 'test',
+        };
+    });
+
+    it('renders formfield component with label', () => {
+        const label = () => <div>label</div>;
+
+        render(
+            <Formik
+                initialValues={{ foobar: '' }}
+                onSubmit={jest.fn()}
+                render={() => <FormField {...defaultProps} label={label} />}
+            />,
+        );
+
+        expect(screen.getByText('label')).toBeInTheDocument();
+    });
+
+    it('renders formfield component with label content', () => {
+        const labelContent = () => <div>label content</div>;
+
+        render(
+            <Formik
+                initialValues={{ foobar: '' }}
+                onSubmit={jest.fn()}
+                render={() => <FormField {...defaultProps} labelContent={labelContent()} />}
+            />,
+        );
+
+        expect(screen.getByText('label content')).toBeInTheDocument();
+    });
+});
---
+++ packages/ui/src/form/FormFieldContainer/FormFieldContainer.test.tsx
@@ -0,0 +1,29 @@
+import { Formik } from 'formik';
+import React from 'react';
+
+import { render, screen } from '@bigcommerce/checkout/test-utils';
+
+import FormFieldContainer, { FormFieldContainerProps } from './FormFieldContainer';
+
+describe('FormFieldContainer', () => {
+    let defaultProps: FormFieldContainerProps;
+
+    beforeEach(() => {
+        defaultProps = {
+            additionalClassName: 'test',
+            children: <div>test</div>,
+        };
+    });
+
+    it('renders formfieldcontainer component', () => {
+        render(
+            <Formik
+                initialValues={{ foobar: '' }}
+                onSubmit={jest.fn()}
+                render={() => <FormFieldContainer {...defaultProps} />}
+            />,
+        );
+
+        expect(screen.getByText('test')).toBeInTheDocument();
+    });
+});
