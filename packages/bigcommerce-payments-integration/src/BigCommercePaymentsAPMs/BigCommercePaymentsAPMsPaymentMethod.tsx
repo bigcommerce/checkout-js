a7f5825cd6ea9e789d2374700c4435a1b8cf4687
andrii.vitvitskyi <andrii.vitvitskyi@bigcommerce.com>
2025-05-12 13:18:33 +0300

    refactor(payment): Added BigCommerce payments components

---
+++ packages/bigcommerce-payments-integration/src/BigCommercePaymentsAPMs/BigCommercePaymentsAPMsPaymentMethod.tsx
@@ -0,0 +1,69 @@
+import React, { FunctionComponent } from 'react';
+
+import {
+    getUniquePaymentMethodId,
+    PaymentMethodProps,
+    PaymentMethodResolveId,
+    toResolvableComponent
+} from '@bigcommerce/checkout/payment-integration-api';
+
+import BigCommercePaymentsPaymentMethodComponent from '../components/BigCommercePaymentsPaymentMethodComponent';
+
+const BigCommercePaymentsAPMsPaymentMethod: FunctionComponent<PaymentMethodProps> = props => {
+    const { method } = props;
+    const isPaymentDataRequired = props.checkoutState.data.isPaymentDataRequired();
+
+    if (!isPaymentDataRequired) {
+        return null;
+    }
+
+
+    const widgetContainerId = getUniquePaymentMethodId(method.id, method.gateway);
+    const extraOptions = {
+        apmFieldsContainer: `#${widgetContainerId}`,
+        apmFieldsStyles: {
+            variables: {
+                fontFamily: 'Open Sans, Helvetica Neue, Arial, sans-serif',
+                colorBackground: 'transparent',
+                textColor: 'black',
+                fontSizeBase: '16px',
+                spacingUnit: '1rem',
+                borderColor: '#d9d9d9',
+                borderRadius: '4px',
+                borderWidth: '1px',
+            },
+            rules: {
+                '.Input': {
+                    backgroundColor: 'white',
+                    color: '#333',
+                    fontSize: '1rem',
+                },
+                '.Input:active': {
+                    color: '#4496f6',
+                },
+                '.Input--invalid': {
+                    color: '#ed6a6a',
+                },
+            },
+        },
+    };
+
+    return (
+        <BigCommercePaymentsPaymentMethodComponent
+            providerOptionsKey="bigcommerce_payments_apms"
+            providerOptionsData={extraOptions}
+            {...props}
+        >
+
+            <div
+                className={`widget widget--${props.method.id} payment-widget`}
+                id={widgetContainerId}
+            ></div>
+        </BigCommercePaymentsPaymentMethodComponent>
+    );
+};
+
+export default toResolvableComponent<PaymentMethodProps, PaymentMethodResolveId>(
+    BigCommercePaymentsAPMsPaymentMethod,
+    [{ gateway: 'bigcommerce_payments_apms' }],
+);
