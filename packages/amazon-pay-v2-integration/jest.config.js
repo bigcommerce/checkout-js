78ea0f63cdb8805d9b5148f937183a5cb54929a6
Richa Patel <richa.patel@bigcommerce.com>
2025-05-13 11:03:12 +1000

    chore(checkout): CHECKOUT-9180 fix PI unit tests

--- packages/amazon-pay-v2-integration/jest.config.js
+++ packages/amazon-pay-v2-integration/jest.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   displayName: 'amazon-pay-v2-integration',
   preset: '../../jest.preset.js',
-  globals: {},
+  transform: {
+    '^.+\\.[tj]sx?$': [
+        'ts-jest',
+        {
+            tsconfig: '<rootDir>/tsconfig.spec.json',
+            diagnostics: false,
+        },
+    ],
+  },
   setupFilesAfterEnv: ['../../jest-setup.ts'],
   coverageDirectory: '../../coverage/packages/amazon-pay-v2-integration',
 };
