497848398f2f6d90bb3af0c63e5da40efb801f95
andrii.vitvitskyi <andrii.vitvitskyi@bigcommerce.com>
2025-05-28 16:40:10 +0300

    refactor(payment): Added BigcommerceCreditBanner

---
+++ packages/paypal-utils/src/BigCommercePaymentsPayLaterBanner.test.tsx
@@ -0,0 +1,77 @@
+import { createCheckoutService } from '@bigcommerce/checkout-sdk';
+import React, { FunctionComponent } from 'react';
+
+import {
+  createLocaleContext,
+  LocaleContext,
+  LocaleContextType,
+} from '@bigcommerce/checkout/locale';
+import { CheckoutProvider, PaymentMethodId } from '@bigcommerce/checkout/payment-integration-api';
+import { getStoreConfig } from '@bigcommerce/checkout/test-mocks';
+import { render } from '@bigcommerce/checkout/test-utils';
+
+import BigCommercePaymentsPayLaterBanner from './BigCommercePaymentsPayLaterBanner';
+
+describe('BigCommercePaymentsPayLaterBanner', () => {
+  let BigCommercePaymentsCreditBannerTest: FunctionComponent;
+  let localeContext: LocaleContextType;
+
+  const checkoutService = createCheckoutService();
+  const checkoutState = checkoutService.getState();
+  const defaultProps = {
+    onUnhandledError: jest.fn(),
+  };
+
+  beforeEach(() => {
+    localeContext = createLocaleContext(getStoreConfig());
+
+    jest.spyOn(checkoutService, 'deinitializePayment').mockResolvedValue(checkoutState);
+
+    jest.spyOn(checkoutService, 'initializePayment').mockResolvedValue(checkoutState);
+
+    BigCommercePaymentsCreditBannerTest = () => (
+      <CheckoutProvider checkoutService={checkoutService}>
+        <LocaleContext.Provider value={localeContext}>
+          <BigCommercePaymentsPayLaterBanner {...defaultProps} />
+        </LocaleContext.Provider>
+      </CheckoutProvider>
+    );
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.resetAllMocks();
+  });
+
+  it('initializes BigCommercePaymentsPayLaterBanner', () => {
+    render(<BigCommercePaymentsCreditBannerTest />);
+
+    expect(checkoutService.initializePayment).toHaveBeenCalledWith({
+      methodId: PaymentMethodId.BigcommercePaymentsPaylater,
+      bigcommerce_payments_paylater: {
+        bannerContainerId: 'bigcommerce-payments-banner-container',
+      },
+    });
+
+    expect(checkoutService.deinitializePayment).toHaveBeenCalledWith({
+      methodId: PaymentMethodId.BigcommercePaymentsPaylater,
+    });
+  });
+
+  it('catches error during BigCommercePaymentsPayLaterBanner initialization', () => {
+    jest.spyOn(checkoutService, 'initializePayment').mockImplementation(() => {
+      throw new Error('something went wrong!');
+    });
+
+    render(<BigCommercePaymentsCreditBannerTest />);
+
+    expect(checkoutService.initializePayment).toHaveBeenCalledWith({
+      methodId: PaymentMethodId.BigcommercePaymentsPaylater,
+      bigcommerce_payments_paylater: {
+        bannerContainerId: 'bigcommerce-payments-banner-container',
+      },
+    });
+
+    expect(defaultProps.onUnhandledError).toHaveBeenCalled();
+  });
+});
--- packages/paypal-utils/src/index.ts
+++ packages/paypal-utils/src/index.ts
@@ -1,2 +1,3 @@
 export { default as PaypalCommerceCreditBanner } from './PaypalCommerceCreditBanner';
 export { default as BraintreePaypalCreditBanner } from './BraintreePaypalCreditBanner';
+export { default as BigCommercePaymentsPayLaterBanner } from './BigCommercePaymentsPayLaterBanner';
