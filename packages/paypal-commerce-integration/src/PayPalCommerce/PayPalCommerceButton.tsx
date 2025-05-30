7f82a10d913cc6635945b9014006a4248f02722b
Serhii Tkachenko <serhii.tkachenko@bigcommerce.com>
2025-05-30 16:17:28 +0300

    refactor(payment): created PayPalCommerceButton and PayPalCommerceCredit components in paypal-commerce-integration package

---
+++ packages/paypal-commerce-integration/src/PayPalCommerce/PayPalCommerceButton.tsx
@@ -0,0 +1,25 @@
+import React, { FunctionComponent } from 'react';
+
+import { CheckoutButton } from '@bigcommerce/checkout/checkout-button-integration';
+import {
+    CheckoutButtonProps,
+    CheckoutButtonResolveId,
+    toResolvableComponent,
+} from '@bigcommerce/checkout/payment-integration-api';
+import { navigateToOrderConfirmation } from '@bigcommerce/checkout/utility';
+
+const PayPalCommerceButton: FunctionComponent<CheckoutButtonProps> = (props) => {
+    const additionalInitializationOptions = {
+        onComplete: navigateToOrderConfirmation,
+        onError: props.onUnhandledError,
+    };
+
+    return <CheckoutButton additionalInitializationOptions={additionalInitializationOptions} {...props} />;
+};
+
+export default toResolvableComponent<CheckoutButtonProps, CheckoutButtonResolveId>(
+    PayPalCommerceButton,
+    [
+        { id: 'paypalcommerce' },
+    ],
+);
---
+++ packages/paypal-commerce-integration/src/PayPalCommerceCredit/PayPalCommerceCreditButton.test.tsx
@@ -0,0 +1,51 @@
+import {
+    CheckoutSelectors,
+    CheckoutService,
+    createCheckoutService,
+    createLanguageService,
+} from '@bigcommerce/checkout-sdk';
+import React from 'react';
+
+import { CheckoutButtonProps } from '@bigcommerce/checkout/payment-integration-api';
+import { render } from '@bigcommerce/checkout/test-utils';
+
+import PayPalCommerceCreditButton from './PayPalCommerceCreditButton';
+
+describe('PayPalCommerceCreditButton', () => {
+    let defaultProps: CheckoutButtonProps;
+    let checkoutService: CheckoutService;
+    let checkoutState: CheckoutSelectors;
+
+    beforeEach(() => {
+        checkoutService = createCheckoutService();
+        checkoutState = checkoutService.getState();
+
+        jest.spyOn(checkoutService, 'initializeCustomer').mockResolvedValue(checkoutState);
+        jest.spyOn(checkoutService, 'deinitializeCustomer').mockResolvedValue(checkoutState);
+
+        defaultProps = {
+            checkoutService,
+            checkoutState,
+            containerId: 'paypalcommercecredit-button-container',
+            language: createLanguageService(),
+            methodId: 'paypalcommercecredit',
+            onUnhandledError: jest.fn(),
+            onWalletButtonClick: jest.fn(),
+        };
+    });
+
+    it('renders PayPalCommerceCreditButton with provided props', () => {
+        render(<PayPalCommerceCreditButton {...defaultProps} />);
+
+        expect(checkoutService.initializeCustomer).toHaveBeenCalledWith({
+            methodId: defaultProps.methodId,
+            paypalcommercecredit: {
+                container: "paypalcommercecredit-button-container",
+                onClick: expect.any(Function),
+                onComplete: expect.any(Function),
+                onError: expect.any(Function),
+                onUnhandledError: expect.any(Function),
+            },
+        });
+    });
+});
