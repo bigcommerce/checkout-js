c8ca7474b454699b2cc0bca3811616a1b11187bf
Serhii Tkachenko <serhii.tkachenko@bigcommerce.com>
2025-05-30 16:17:28 +0300

    refactor(payment): created PayPalCommerceButton and PayPalCommerceCredit components in paypal-commerce-integration package

---
+++ packages/paypal-commerce-integration/src/PayPalCommerce/PayPalCommerceButton.test.tsx
@@ -0,0 +1,51 @@
+import {
+    CheckoutSelectors,
+    CheckoutService,
+    createCheckoutService,
+    createLanguageService,
+} from '@bigcommerce/checkout-sdk';
+import React from 'react';
+
+import { CheckoutButtonProps } from '@bigcommerce/checkout/payment-integration-api';
+import { render } from '@bigcommerce/checkout/test-utils';
+
+import PayPalCommerceButton from './PayPalCommerceButton';
+
+describe('PayPalCommerceButton', () => {
+    let defaultProps: CheckoutButtonProps;
+    let checkoutService: CheckoutService;
+    let checkoutState: CheckoutSelectors;
+
+    beforeEach(() => {
+        checkoutService = createCheckoutService();
+        checkoutState = checkoutService.getState();
+
+        jest.spyOn(checkoutService, 'initializeCustomer').mockResolvedValue(checkoutState);
+        jest.spyOn(checkoutService, 'deinitializeCustomer').mockResolvedValue(checkoutState);
+
+        defaultProps = {
+            checkoutService,
+            checkoutState,
+            containerId: 'paypalcommerce-button-container',
+            language: createLanguageService(),
+            methodId: 'paypalcommerce',
+            onUnhandledError: jest.fn(),
+            onWalletButtonClick: jest.fn(),
+        };
+    });
+
+    it('renders PayPalCommerceButton with provided props', () => {
+        render(<PayPalCommerceButton {...defaultProps} />);
+
+        expect(checkoutService.initializeCustomer).toHaveBeenCalledWith({
+            methodId: defaultProps.methodId,
+            paypalcommerce: {
+                container: "paypalcommerce-button-container",
+                onClick: expect.any(Function),
+                onComplete: expect.any(Function),
+                onError: expect.any(Function),
+                onUnhandledError: expect.any(Function),
+            },
+        });
+    });
+});
--- packages/paypal-commerce-integration/src/index.ts
+++ packages/paypal-commerce-integration/src/index.ts
@@ -1,6 +1,43 @@
+/**
+ *
+ * PayPal Commerce APMs
+ *
+ * */
 export { default as PayPalCommerceAPMsPaymentMethod } from './PayPalCommerceAPMs/PayPalCommerceAPMsPaymentMethod';
+
+/**
+ *
+ * PayPal Commerce Credit
+ *
+ * */
+export { default as PayPalCommerceCreditButton } from './PayPalCommerceCredit/PayPalCommerceCreditButton';
 export { default as PayPalCommerceCreditPaymentMethod } from './PayPalCommerceCredit/PayPalCommerceCreditPaymentMethod';
+
+/**
+ *
+ * PayPal Commerce Fastlane
+ *
+ * */
 export { default as PayPalCommerceFastlanePaymentMethod } from './PayPalCommerceFastlane';
+
+/**
+ *
+ * PayPal Commerce (PayPal)
+ *
+ * */
+export { default as PayPalCommerceButton } from './PayPalCommerce/PayPalCommerceButton';
 export { default as PayPalCommercePaymentMethod } from './PayPalCommerce/PayPalCommercePaymentMethod';
+
+/**
+ *
+ * PayPal Commerce Venmo
+ *
+ * */
 export { default as PayPalCommerceVenmoPaymentMethod } from './PayPalCommerceVenmo/PayPalCommerceVenmoPaymentMethod';
+
+/**
+ *
+ * PayPal Commerce RatePay
+ *
+ * */
 export { default as PaypalCommerceRatePayPaymentMethod } from './PayPalCommerceRatepay/PaypalCommerceRatePayPaymentMethod';
