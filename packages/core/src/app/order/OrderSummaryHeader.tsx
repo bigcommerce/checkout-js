71095c890de9753c4caa4830bc471f5f5587f17e
Richa Patel <richa.patel@bigcommerce.com>
2025-06-02 11:38:28 +1000

    feat(checkout): update font style for checkout page -WIP

--- packages/core/src/app/order/OrderSummaryHeader.tsx
+++ packages/core/src/app/order/OrderSummaryHeader.tsx
@@ -1,10 +1,14 @@
 import React, { FunctionComponent } from 'react';
 
 import { TranslatedString } from '@bigcommerce/checkout/locale';
+import classNames from 'classnames';
+
+const newFontStyle = true; // Assuming this is a placeholder for the actual condition
 
 const OrderSummaryHeader: FunctionComponent<{ children?: React.ReactNode }> = ({ children }) => (
     <header className="cart-header">
-        <h3 className="cart-title optimizedCheckout-headingSecondary">
+        <h3 className={classNames('cart-title', 'optimizedCheckout-headingSecondary',
+            { 'header-secondary': newFontStyle })}>
             <TranslatedString id="cart.cart_heading" />
         </h3>
         {children}
