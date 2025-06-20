47adbcf428c38c86e893791444394168937e8150
Richa Patel <richa.patel@bigcommerce.com>
2025-06-20 17:04:51 +1000

    feat(extension): introduce new ReRenderShippingStep command

--- packages/checkout-extension/src/handler/commandHandlers/index.ts
+++ packages/checkout-extension/src/handler/commandHandlers/index.ts
@@ -2,3 +2,4 @@ export { createReloadCheckoutHandler } from './createReloadCheckoutHandler';
 export { createSetIframeStyleHandler } from './createSetIframeStyleHandler';
 export { createShowLoadingIndicatorHandler } from './createShowLoadingIndicatorHandler';
 export { createReRenderShippingFormHandler } from './createReRenderShippingFormHandler';
+export { createReRenderShippingStepHandler } from './createReRenderShippingStepHandler';
