7ac073dfd610eee541249228c25a2a02c0433ee6
Serhii Tkachenko <serhii.tkachenko@bigcommerce.com>
2025-06-24 16:39:31 +0300

    feat(payment): updated hosted credit card integration with card expiry field logic

--- packages/hosted-credit-card-integration/src/components/HostedCreditCardValidation/HostedCreditCardValidation.tsx
+++ packages/hosted-credit-card-integration/src/components/HostedCreditCardValidation/HostedCreditCardValidation.tsx
@@ -2,17 +2,23 @@ import React, { FunctionComponent } from 'react';
 
 import { TranslatedString } from '@bigcommerce/checkout/locale';
 
-import { HostedCreditCardCodeField, HostedCreditCardNumberField } from '../';
+import {
+    HostedCreditCardCodeField,
+    HostedCreditCardExpiryField,
+    HostedCreditCardNumberField,
+} from '../';
 
 export interface HostedCreditCardValidationProps {
     cardCodeId?: string;
     cardNumberId?: string;
+    cardExpiryId?: string;
     focusedFieldType?: string;
 }
 
 const HostedCreditCardValidation: FunctionComponent<HostedCreditCardValidationProps> = ({
     cardCodeId,
     cardNumberId,
+    cardExpiryId,
     focusedFieldType,
 }) => (
     <>
@@ -42,6 +48,14 @@ const HostedCreditCardValidation: FunctionComponent<HostedCreditCardValidationPr
                     appearFocused={focusedFieldType === 'cardCode'}
                     id={cardCodeId}
                     name="hostedForm.errors.cardCodeVerification"
+                />
+            )}
+
+            {cardExpiryId && (
+                <HostedCreditCardExpiryField
+                    appearFocused={focusedFieldType === 'cardExpiry'}
+                    id={cardExpiryId}
+                    name="hostedForm.errors.cardExpiryVerification"
                 />
             )}
         </div>
