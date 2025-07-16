362b57c62f177bdea3612306f8363a95853e319d
Peng Zhou <peng.zhou@bigcommerce.com>
2025-07-16 15:23:40 +1000

    feat(checkout): CHECKOUT-9376 Update Autocomplete for AU Address

--- packages/core/src/app/address/googleAutocomplete/AddressSelectorAU.test.ts
+++ packages/core/src/app/address/googleAutocomplete/AddressSelectorAU.test.ts
@@ -5,12 +5,12 @@ describe('AddressSelectorAU', () => {
     it('should return correct street address', () => {
         const selector = new AddressSelectorAU(getGoogleAutocompletePlaceMock());
 
-        expect(selector.getStreet()).toBe('1-3 (l) Smail Street');
+        expect(selector.getStreet()).toBe('unit 6 1-3 (l) Smail Street');
     });
 
     it('should return correct street2 address value', () => {
         const selector = new AddressSelectorAU(getGoogleAutocompletePlaceMock());
 
-        expect(selector.getStreet2()).toBe('unit 6');
+        expect(selector.getStreet2()).toBe('');
     });
 });
