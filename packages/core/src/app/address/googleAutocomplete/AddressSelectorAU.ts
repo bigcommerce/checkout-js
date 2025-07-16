228acafbedfad766da1610bc419c25debad3ae57
Peng Zhou <88361607+bc-peng@users.noreply.github.com>
2025-07-17 09:41:17 +1000

    feat(checkout): CHECKOUT-9376 Update Autocomplete for AU Address (#2431)

--- packages/core/src/app/address/googleAutocomplete/AddressSelectorAU.ts
+++ packages/core/src/app/address/googleAutocomplete/AddressSelectorAU.ts
@@ -2,10 +2,10 @@ import AddressSelector from './AddressSelector';
 
 export default class AddressSelectorAU extends AddressSelector {
     getStreet(): string {
-        return `${this._get('street_number', 'long_name')} ${this._get('route', 'long_name')}`;
+        return `${this._get('subpremise', 'long_name')} ${this._get('street_number', 'long_name')} ${this._get('route', 'long_name')}`;
     }
 
     getStreet2(): string {
-        return this._get('subpremise', 'long_name');
+        return '';
     }
 }
