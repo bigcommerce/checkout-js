eb357549f02b7cbcf93654bb26c115e2d55b777d
Richa Patel <141614330+richapatel1510@users.noreply.github.com>
2025-07-21 11:16:56 +1000

    Fix billing same as shipping checkbox to honour checkout settings (#2435)
    * fix(checkout): render checkout only after config and checkout data is loaded
    
    * fix(checkout): fix lint
    
    * fix(checkout): add test
    
    * fix(checkout): add comment for enableReinitialize in shipping form
    
    * fix(checkout): fix lint

--- packages/core/src/app/checkout/Checkout.tsx
+++ packages/core/src/app/checkout/Checkout.tsx
@@ -147,7 +147,6 @@ export interface WithCheckoutProps {
     isPriceHiddenFromGuests: boolean;
     isShowingWalletButtonsOnTop: boolean;
     isShippingDiscountDisplayEnabled: boolean;
-    isInitialLoadFinished: boolean;
     loginUrl: string;
     cartUrl: string;
     createAccountUrl: string;
@@ -192,12 +191,6 @@ class Checkout extends Component<
     }
 
     componentDidUpdate(prevProps: WithCheckoutProps): void {
-        const { isInitialLoad } = this.state;
-
-        if (this.props.isInitialLoadFinished && isInitialLoad) {
-            this.setState({ isInitialLoad: false });
-        }
-        
         if (prevProps.steps.length === 0 && this.props.steps && this.props.steps.length > 0) {
             this.handleReady();
         }
@@ -296,6 +289,10 @@ class Checkout extends Component<
                 this.setState({ isMultiShippingMode });
             }
 
+            if (this.state.isInitialLoad) {
+                this.setState({ isInitialLoad: false });
+            }
+
             window.addEventListener('beforeunload', this.handleBeforeExit);
 
         } catch (error) {
@@ -350,7 +347,7 @@ class Checkout extends Component<
         const loadingSkeleton = isMobileView() ? null : pageLoadingSkeleton;
 
         return (
-            <LoadingOverlay hideContentWhenLoading isLoading={isInitialLoad || isRedirecting} loadingSkeleton={loadingSkeleton}>
+            <LoadingOverlay isLoading={isInitialLoad || isRedirecting} loadingSkeleton={loadingSkeleton} unmountContentWhenLoading>
                 <div className="layout-main">
                     <LoadingNotification isLoading={extensionState.isShowingLoadingIndicator} />
 
