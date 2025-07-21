dff47e7d40a7508f906b586bae15e9fbf2550612
andrii.vitvitskyi <andrii.vitvitskyi@bigcommerce.com>
2025-07-21 18:19:27 +0300

    feat(payment): Added onError handler for PPCP FL

--- packages/paypal-commerce-integration/src/PayPalCommerceFastlane/PayPalCommerceFastlanePaymentMethod.test.tsx
+++ packages/paypal-commerce-integration/src/PayPalCommerceFastlane/PayPalCommerceFastlanePaymentMethod.test.tsx
@@ -47,6 +47,7 @@ describe('PayPalCommerceFastlanePaymentMethod', () => {
             paypalcommercefastlane: {
                 onInit: expect.any(Function),
                 onChange: expect.any(Function),
+                onError: expect.any(Function),
             },
         });
     });
---
+++ packages/paypal-commerce-integration/src/PayPalCommerceFastlane/is-error-with-translation-key.ts
@@ -0,0 +1,14 @@
+
+export interface ErrorWithTranslationKey {
+  translationKey: string;
+}
+
+export default function isErrorWithTranslationKey(
+  error: unknown,
+): error is ErrorWithTranslationKey {
+  return (
+    typeof error === 'object' &&
+    error !== null &&
+    'translationKey' in error
+  );
+}
