031225023f346e1e0e0fae6f34329a040672b6ca
Peng Zhou <88361607+bc-peng@users.noreply.github.com>
2025-07-22 11:21:32 +1000

    feat(checkout): CHECKOUT-9358 Hide "Edit Cart" in Mobile Buy Now Cart (#2447)
    feat(checkout): CHECKOUT-9358 Hide "Edit Cart" in Mobile Buy Now Cart

--- packages/core/src/app/cart/CartSummaryDrawer.test.tsx
+++ packages/core/src/app/cart/CartSummaryDrawer.test.tsx
@@ -13,6 +13,12 @@ import { getCustomer } from '../customer/customers.mock';
 import CartSummaryDrawer from './CartSummaryDrawer';
 
 describe('CartSummary Component', () => {
+    Object.defineProperty(window, 'location', {
+        value: {
+            pathname: '/checkout',
+        },
+    });
+
     it('renders OrderSummaryDrawer with Edit Cart link', async () => {
         const checkoutService = createCheckoutService();
         const localeContext = createLocaleContext(getStoreConfig());
@@ -36,5 +42,34 @@ describe('CartSummary Component', () => {
         expect(screen.getByText(
             localeContext.language.translate('cart.edit_cart_action'),
         )).toBeInTheDocument();
+    });
+
+    it('renders OrderSummaryDrawer without Edit Cart link for Buy Now carts', async () => {
+        const checkoutService = createCheckoutService();
+        const localeContext = createLocaleContext(getStoreConfig());
+
+        jest.spyOn(checkoutService.getState().data, 'getCustomer').mockReturnValue(getCustomer());
+        jest.spyOn(checkoutService.getState().data, 'getCheckout').mockReturnValue({...getCheckout(),
+        cart: {
+            ...getCheckout().cart,
+            source: "BUY_NOW",
+        }});
+        jest.spyOn(checkoutService.getState().data, 'getConfig').mockReturnValue(getStoreConfig());
+
+        render(
+            <CheckoutProvider checkoutService={checkoutService}>
+                <LocaleContext.Provider value={localeContext}>
+                    <CartSummaryDrawer isMultiShippingMode={false} />
+                </LocaleContext.Provider>
+            </CheckoutProvider>,
+        );
+
+        await userEvent.click(screen.getByText(
+            localeContext.language.translate('cart.show_details_action'),
+        ));
+
+        expect(
+          screen.queryByText(localeContext.language.translate('cart.edit_cart_action')),
+        ).not.toBeInTheDocument();
     });
 });
