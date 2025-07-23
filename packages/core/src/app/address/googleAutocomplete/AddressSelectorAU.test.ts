97c34fbe4d5950120d9ae449b349ecaeca542f50
Peng Zhou <peng.zhou@bigcommerce.com>
2025-07-23 10:53:32 +1000

    feat(checkout): CHECKOUT-9376 Update Autocomplete for AU Address II

--- packages/core/src/app/address/googleAutocomplete/AddressSelectorAU.test.ts
+++ packages/core/src/app/address/googleAutocomplete/AddressSelectorAU.test.ts
@@ -5,7 +5,7 @@ describe('AddressSelectorAU', () => {
     it('should return correct street address', () => {
         const selector = new AddressSelectorAU(getGoogleAutocompletePlaceMock());
 
-        expect(selector.getStreet()).toBe('unit 6 1-3 (l) Smail Street');
+        expect(selector.getStreet()).toBe('unit 6/1-3 (l) Smail Street');
     });
 
     it('should return correct street2 address value', () => {
--- packages/core/src/app/address/googleAutocomplete/AddressSelectorAU.ts
+++ packages/core/src/app/address/googleAutocomplete/AddressSelectorAU.ts
@@ -2,7 +2,10 @@ import AddressSelector from './AddressSelector';
 
 export default class AddressSelectorAU extends AddressSelector {
     getStreet(): string {
-        return `${this._get('subpremise', 'long_name')} ${this._get('street_number', 'long_name')} ${this._get('route', 'long_name')}`;
+        const subpremise = this._get('subpremise', 'short_name');
+        const subpremisePart = subpremise ? `${subpremise}/` : '';
+
+        return `${subpremisePart}${this._get('street_number', 'long_name')} ${this._get('route', 'long_name')}`;
     }
 
     getStreet2(): string {
