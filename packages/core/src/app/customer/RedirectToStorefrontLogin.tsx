cdc645f51a9bc429c45487f27856700add591710
animesh1987 <animeshsingh1987@gmail.com>
2025-08-11 16:16:49 +1000

    feat(customer): CHECKOUT-9390 Redirect to storefront for auth when setting is on

---
+++ packages/core/src/app/customer/RedirectToStorefrontLogin.tsx
@@ -0,0 +1,42 @@
+import { TranslatedString } from '@bigcommerce/checkout/locale';
+import { useCheckout } from '@bigcommerce/checkout/payment-integration-api';
+import { Button, ButtonVariant, useThemeContext } from '@bigcommerce/checkout/ui';
+import React from 'react';
+
+interface RedirectToStorefrontLoginProps {
+    isDisabled: boolean;
+    isLoading: boolean;
+}
+
+export const RedirectToStorefrontLogin: React.FC<RedirectToStorefrontLoginProps> = ({
+    isDisabled,
+    isLoading,
+}) => {
+    const { themeV2 } = useThemeContext();
+    const { checkoutState: { data: { getConfig } } } = useCheckout();
+
+    const config = getConfig();
+    if (!config) {
+        return null;
+    }
+
+    const { checkoutLink, loginLink } = config.links;
+
+    const handleRedirect = () => {
+        return window.location.assign(`${loginLink}?redirectTo=${checkoutLink}`);
+    }
+
+    return (
+        <Button
+            className={themeV2 ? 'body-bold' : ''}
+            disabled={isDisabled}
+            id="checkout-customer-continue"
+            isLoading={isLoading}
+            onClick={handleRedirect}
+            testId="customer-continue-button"
+            variant={ButtonVariant.Primary}
+        >
+            <TranslatedString id="customer.sign_in_action" />
+        </Button>
+    );
+};
