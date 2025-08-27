af92e6f42e92283bb201092555cc8a8815ed7366
Richa Patel <richa.patel@bigcommerce.com>
2025-08-27 16:42:45 +1000

    feat(checkout): CHECKOUT-9454 move types to separate file

--- packages/core/src/app/address/AddressForm.tsx
+++ packages/core/src/app/address/AddressForm.tsx
@@ -1,4 +1,4 @@
-import { type Address, type Country, type FormField } from '@bigcommerce/checkout-sdk';
+import { type FormField } from '@bigcommerce/checkout-sdk';
 import { forIn, noop } from 'lodash';
 import React, { useCallback, useContext, useEffect, useRef } from 'react';
 
@@ -8,67 +8,15 @@ import { DynamicFormField, DynamicFormFieldType, ThemeContext } from '@bigcommer
 import { type AutocompleteItem } from '../ui/autocomplete';
 import { CheckboxFormField, Fieldset } from '../ui/form';
 
-import { type AddressKeyMap } from './address';
+import { type AddressFormProps, AUTOCOMPLETE, AUTOCOMPLETE_FIELD_NAME, LABEL, PLACEHOLDER } from './AddressFormType';
 import {
     getAddressFormFieldInputId,
     getAddressFormFieldLegacyName,
 } from './getAddressFormFieldInputId';
 import { GoogleAutocompleteFormField, mapToAddress } from './googleAutocomplete';
-import './AddressForm.scss';
+import './AddressForm.scss'; 
 
-export interface AddressFormProps {
-    fieldName?: string;
-    countryCode?: string;
-    countriesWithAutocomplete?: string[];
-    countries?: Country[];
-    formFields: FormField[];
-    googleMapsApiKey?: string;
-    shouldShowSaveAddress?: boolean;
-    isFloatingLabelEnabled?: boolean;
-    onAutocompleteSelect?(address: Partial<Address>): void;
-    onAutocompleteToggle?(state: { inputValue: string; isOpen: boolean }): void;
-    onChange?(fieldName: string, value: string | string[]): void;
-    setFieldValue?(fieldName: string, value: string | string[]): void;
-}
-
-const LABEL: AddressKeyMap = {
-    address1: 'address.address_line_1_label',
-    address2: 'address.address_line_2_label',
-    city: 'address.city_label',
-    company: 'address.company_name_label',
-    countryCode: 'address.country_label',
-    firstName: 'address.first_name_label',
-    lastName: 'address.last_name_label',
-    phone: 'address.phone_number_label',
-    postalCode: 'address.postal_code_label',
-    stateOrProvince: 'address.state_label',
-    stateOrProvinceCode: 'address.state_label',
-};
-
-const AUTOCOMPLETE: AddressKeyMap = {
-    address1: 'address-line1',
-    address2: 'address-line2',
-    city: 'address-level2',
-    company: 'organization',
-    countryCode: 'country',
-    firstName: 'given-name',
-    lastName: 'family-name',
-    phone: 'tel',
-    postalCode: 'postal-code',
-    stateOrProvince: 'address-level1',
-    stateOrProvinceCode: 'address-level1',
-};
-
-const PLACEHOLDER: AddressKeyMap = {
-    countryCode: 'address.select_country_action',
-    stateOrProvince: 'address.select_state_action',
-    stateOrProvinceCode: 'address.select_state_action',
-};
-
-const AUTOCOMPLETE_FIELD_NAME = 'address1';
-
-const AddressForm: React.FC<AddressFormProps> = (props) => {
-    const {
+const AddressForm: React.FC<AddressFormProps> = ({
         formFields,
         fieldName,
         countriesWithAutocomplete,
@@ -80,8 +28,7 @@ const AddressForm: React.FC<AddressFormProps> = (props) => {
         countries,
         setFieldValue = noop,
         onChange = noop,
-    } = props;
-
+    }) => {
     const { language } = useLocale();
     const context = useContext(ThemeContext);
     const containerRef = useRef<HTMLDivElement>(null);
