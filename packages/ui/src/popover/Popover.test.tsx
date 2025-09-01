9c6d8a6918a732cf7303387b157a831dda90d6db
Richa Patel <richa.patel@bigcommerce.com>
2025-09-01 10:41:40 +1000

    feat(checkout): CHECKOUT-9454 copy Popover component to ui package

---
+++ packages/ui/src/popover/Popover.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { render } from '@bigcommerce/checkout/test-utils';
+
+import Popover from './Popover';
+
+describe('Popover Component', () => {
+    it('renders with whatever child is passed', () => {
+        const { container } = render(<Popover><h1>Test</h1></Popover>);
+
+        expect(container.innerHTML).toContain("<h1>Test</h1>");
+    });
+});
---
+++ packages/ui/src/popover/PopoverList.test.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import { render, screen } from '@bigcommerce/checkout/test-utils';
+
+import PopoverList from './PopoverList';
+
+describe('Popover Component', () => {
+    const items = [
+        { content: 'Lorem', id: 'x' },
+        { content: 'Ipsum', id: 'y' },
+        { content: 'Foo', id: 'z' },
+    ];
+
+    it('renders list with passed items', () => {
+        render(<PopoverList items={items} />);
+
+        expect(screen.getByText('Lorem')).toBeInTheDocument();
+        expect(screen.getByText('Ipsum')).toBeInTheDocument();
+        expect(screen.getByText('Foo')).toBeInTheDocument();
+    });
+
+    it('renders empty list when empty array is passed', () => {
+        const { container } = render(<PopoverList items={[]} />);
+
+        // eslint-disable-next-line testing-library/no-container
+        expect(container.getElementsByClassName('popoverList-item')).toHaveLength(0);
+    });
+
+    it('renders list with highlighted item', () => {
+        const { container } = render(<PopoverList highlightedIndex={1} items={items} />);
+
+        // eslint-disable-next-line testing-library/no-container
+        const popoverListItems = container.getElementsByClassName('popoverList-item')
+
+        expect(popoverListItems).toHaveLength(3);
+
+        expect(popoverListItems[0]).not.toHaveClass('is-active');
+        expect(popoverListItems[1]).toHaveClass('is-active');
+        expect(popoverListItems[2]).not.toHaveClass('is-active');
+    });
+
+    it('renders list with passed testId', () => {
+        render(<PopoverList items={items} testId='testId' />);
+
+        expect(screen.getByTestId('testId')).toBeInTheDocument();
+    });
+});
