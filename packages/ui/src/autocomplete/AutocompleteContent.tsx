5dbeb5128fca0ab1a2aa73477f4f15da70062269
Richa Patel <141614330+richapatel1510@users.noreply.github.com>
2025-09-08 11:29:28 +1000

    feat(checkout): CHECKOUT-9454 Convert Autocomplete component (#2543)
    * feat(checkout): CHECKOUT-9454 convert and refactor Autocomplete component
    
    * feat(checkout): CHECKOUT-9454 copy Autocomplete component to ui package
    
    * feat(checkout): CHECKOUT-9454 copy Popover component to ui package
    
    * feat(checkout): CHECKOUT-9454 fix lint
    
    * feat(checkout): CHECKOUT-9454 fix lint

---
+++ packages/ui/src/autocomplete/AutocompleteContent.tsx
@@ -0,0 +1,91 @@
+import type { GetInputPropsOptions, GetItemPropsOptions, GetMenuPropsOptions } from 'downshift';
+import { includes, isNumber } from 'lodash';
+import React, { type ReactNode } from 'react';
+
+import { useThemeContext } from '@bigcommerce/checkout/ui';
+
+import { Label } from '../form';
+import { Popover, PopoverList } from '../popover';
+
+import { type InputPropsType } from './Autocomplete';
+import type AutocompleteItem from './autocomplete-item';
+import { toPopoverItem } from './utils';
+
+export interface AutocompleteContentProps {
+    isOpen: boolean;
+    getInputProps: (options?: GetInputPropsOptions) => React.InputHTMLAttributes<HTMLInputElement>;
+    getMenuProps: (options?: GetMenuPropsOptions) => React.HTMLAttributes<HTMLElement>;
+    getItemProps: (
+        options: GetItemPropsOptions<AutocompleteItem>,
+    ) => React.HTMLAttributes<HTMLElement>;
+    highlightedIndex: number | null;
+    initialValue?: string;
+    inputProps?: InputPropsType;
+    items: AutocompleteItem[];
+    listTestId?: string;
+    children?: ReactNode;
+}
+
+const AutocompleteContent: React.FC<AutocompleteContentProps> = ({
+    isOpen,
+    getInputProps,
+    getMenuProps,
+    getItemProps,
+    highlightedIndex,
+    initialValue,
+    inputProps,
+    items,
+    listTestId,
+    children,
+}) => {
+    const { themeV2 } = useThemeContext();
+    const baseInputProps = getInputProps({ value: initialValue });
+    const combinedProps = { ...baseInputProps, ...inputProps };
+
+    // Extract labelText to avoid passing it to input element
+    const { labelText: _labelText, ...validInputProps } = combinedProps;
+
+    const getProps = (index: number, itemId: string) => {
+        const autocompleteItem = items.find((item) => item.id === itemId);
+
+        if (!autocompleteItem) {
+            return {};
+        }
+
+        return getItemProps({
+            key: itemId,
+            index,
+            item: autocompleteItem,
+        });
+    };
+
+    return (
+        <>
+            <input {...validInputProps} />
+            {inputProps && includes(inputProps.className, 'floating') && (
+                <Label
+                    additionalClassName={themeV2 ? 'floating-form-field-label' : ''}
+                    htmlFor={inputProps.id}
+                    id={inputProps['aria-labelledby']}
+                    isFloatingLabelEnabled={true}
+                >
+                    {inputProps.labelText}
+                </Label>
+            )}
+            {isOpen && !!items.length && (
+                <Popover>
+                    <PopoverList
+                        getItemProps={getProps}
+                        highlightedIndex={isNumber(highlightedIndex) ? highlightedIndex : -1}
+                        items={items.map((item) => toPopoverItem(item))}
+                        menuProps={getMenuProps()}
+                        testId={listTestId}
+                    />
+                    {children}
+                </Popover>
+            )}
+        </>
+    );
+};
+
+export default AutocompleteContent;
