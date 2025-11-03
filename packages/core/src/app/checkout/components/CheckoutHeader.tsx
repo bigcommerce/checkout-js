eeafb9e48a1f81648f8235d013cd02d900da8c17
animesh1987 <animeshsingh1987@gmail.com>
2025-11-03 22:57:51 +1100

    chore(checkout): CHECKOUT-9617 Add checkout header component

---
+++ packages/core/src/app/checkout/components/CheckoutHeader.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { type PaymentMethod } from '@bigcommerce/checkout-sdk/essential';
+import { useCheckout } from '@bigcommerce/checkout/payment-integration-api';
+import { useExtensions } from '@bigcommerce/checkout/contexts';
+import { LoadingNotification } from '@bigcommerce/checkout/ui';
+
+import { CheckoutButtonContainer } from '../../customer';
+import { EMPTY_ARRAY } from '../../common/utility';
+import { PromotionBannerList } from '../../promotion';
+import CheckoutStepType from '../CheckoutStepType';
+
+export interface CheckoutHeaderProps {
+    activeStepType?: CheckoutStepType;
+    defaultStepType?: CheckoutStepType;
+    buttonConfigs: PaymentMethod[];
+    checkEmbeddedSupport: (methodIds: string[]) => boolean;
+    onUnhandledError: (error: Error) => void;
+    onWalletButtonClick: (methodName: string) => void;
+}
+
+export const CheckoutHeader: React.FC<CheckoutHeaderProps> = ({
+    activeStepType,
+    defaultStepType,
+    buttonConfigs,
+    checkEmbeddedSupport,
+    onUnhandledError,
+    onWalletButtonClick,
+}) => {
+    const { checkoutState } = useCheckout();
+    const { extensionState } = useExtensions();
+    
+    const { data } = checkoutState;
+
+    const { promotions = EMPTY_ARRAY } = data.getCheckout() || {};
+
+    const config = data.getConfig();
+    const isShowingWalletButtonsOnTop = Boolean(
+        config?.checkoutSettings?.checkoutUserExperienceSettings?.walletButtonsOnTop
+    );
+
+    const isPaymentStepActive = activeStepType
+        ? activeStepType === CheckoutStepType.Payment
+        : defaultStepType === CheckoutStepType.Payment;
+    
+    return (
+        <>
+            <LoadingNotification isLoading={extensionState.isShowingLoadingIndicator} />
+            <PromotionBannerList promotions={promotions} />
+            {isShowingWalletButtonsOnTop && buttonConfigs?.length > 0 && (
+                <CheckoutButtonContainer
+                    checkEmbeddedSupport={checkEmbeddedSupport}
+                    isPaymentStepActive={isPaymentStepActive}
+                    onUnhandledError={onUnhandledError}
+                    onWalletButtonClick={onWalletButtonClick}
+                />
+            )}
+        </>
+    );
+};
+
+export default CheckoutHeader;
