eb26045639d08f75977630fb711cd4c29ebd8ff8
SerhiiFilkovskyi <serhii.filkovskiy@bigcommerce.com>
2025-11-11 16:57:27 +0200

    fix(payment): PAYPAL-6050 add header Wallet buttons on top of checkout page

--- packages/core/src/app/customer/getSupportedMethods.ts
+++ packages/core/src/app/customer/getSupportedMethods.ts
@@ -5,6 +5,9 @@ export const SUPPORTED_METHODS: string[] = [
     'amazonpay',
     APPLE_PAY,
     'chasepay',
+    'bigcommerce_payments',
+    'bigcommerce_payments_paylater',
+    'bigcommerce_payments_venmo',
     'braintreevisacheckout',
     'braintreepaypal',
     'braintreepaypalcredit',
@@ -27,6 +30,7 @@ export const SUPPORTED_METHODS: string[] = [
     'googlepaytdonlinemart',
     'stripeocs',
     'googlepaystripeocs',
+    'googlepay_bigcommerce_payments',
 ];
 
 export const getSupportedMethodIds = (methodIds: string[]): string[] => {
