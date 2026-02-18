dc70476954fa56a41e37aade586f7c64dd909484
JessHu <138756478+JessicaHu51@users.noreply.github.com>
2026-02-18 17:25:28 +1100

    Update packages/ui/src/animation/collapse/index.ts
    Co-authored-by: Tharaa <36555311+Tharaae@users.noreply.github.com>

--- packages/ui/src/animation/collapse/index.ts
+++ packages/ui/src/animation/collapse/index.ts
@@ -1,4 +1,1 @@
-export { CollapseCSSTransition, type CollapseCSSTransitionProps } from './CollapseCSSTransition';
-export { createCollapseAnimationHandlers } from './createCollapseAnimationHandlers';
-export { createSlideCollapseAnimationHandlers } from './createSlideCollapseAnimationHandlers';
-export type { CollapseAnimationHandlers } from './CollapseAnimationHandlers';
+export { CollapseCSSTransition } from './CollapseCSSTransition';
