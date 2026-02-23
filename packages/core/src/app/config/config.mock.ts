7e2018ae5062a235463b8a0a3fb10094edcec511
animesh1987 <animeshsingh1987@gmail.com>
2026-02-23 16:19:36 +1100

    chore(checkout): CHECKOUT-9332 Check for theme setting flag when enabling theme

--- packages/core/src/app/config/config.mock.ts
+++ packages/core/src/app/config/config.mock.ts
@@ -15,6 +15,7 @@ export function getStoreConfig(): StoreConfig {
             checkoutUserExperienceSettings: {
                 walletButtonsOnTop: false,
                 floatingLabelEnabled: false,
+                checkoutV2Theme: false,
             },
             enableOrderComments: true,
             enableTermsAndConditions: false,
