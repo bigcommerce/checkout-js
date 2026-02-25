844832e19bc07198d1c95876a3b3935ca2bacf18
shawn.wang <shawn.wang@bigcommerce.com>
2026-02-25 12:38:25 +1100

    feat(checkout): Handle cart_stock_positions_changed error from submitting order and render the message modal
    - re-used existing translation key for ``{count} ready to ship` and `{count} will be backordered`

--- packages/core/src/app/payment/CartStockPositionsChangedItem.tsx
+++ packages/core/src/app/payment/CartStockPositionsChangedItem.tsx
@@ -16,76 +16,64 @@ const CartStockPositionsChangedItem: FunctionComponent<CartStockPositionsChanged
     const backorderMessage = stock?.backorderMessage ?? '';
 
     return (
-        <li
-            className="productList-item"
-            data-test="cart-stock-positions-changed-item"
-        >
-            <div className="product">
-                <figure className="product-column product-figure">
-                    {getOrderSummaryItemImage(item)}
-                </figure>
-                <div className="product-column product-body">
-                    <h4
-                        className="product-title optimizedCheckout-contentPrimary body-medium"
-                        data-test="cart-item-product-title"
-                    >
-                        {item.name}
-                    </h4>
-                    {item.options && item.options.length > 0 && (
-                        <ul
-                            className="product-options optimizedCheckout-contentSecondary sub-text-medium"
-                            data-test="cart-item-product-options"
-                        >
-                            {item.options.map((option, index) => (
-                                <li
-                                    className="product-option"
-                                    data-test="cart-item-product-option"
-                                    key={index}
-                                >
-                                    {option.name} {option.value}
-                                </li>
-                            ))}
-                        </ul>
-                    )}
-                    <p
-                        className="optimizedCheckout-contentSecondary sub-text-medium"
-                        data-test="cart-item-quantity"
-                    >
-                        <TranslatedString
-                            data={{ quantity: item.quantity }}
-                            id="cart.qty_label"
-                        />
-                    </p>
-                </div>
-                <div
-                    className="cart-stock-position-details optimizedCheckout-contentSecondary sub-text-medium"
-                    data-test="cart-item-stock-position"
-                >
-                    {quantityOnHand > 0 && (
-                        <span>
-                            <TranslatedString
-                                data={{ count: quantityOnHand }}
-                                id="cart.qty_on_hand_prompt"
-                            />
-                        </span>
-                    )}
-                    {quantityBackordered > 0 && (
-                        <span className="cart-stock-position-backorder">
-                            {' '}
-                            <TranslatedString
-                                data={{ count: quantityBackordered }}
-                                id="cart.qty_on_backorder_prompt"
-                            />
-                        </span>
-                    )}
-                    {backorderMessage && (
-                        <span className="cart-stock-position-backorder-message">
-                            {backorderMessage}
-                        </span>
-                    )}
-                </div>
-            </div>
-        </li>
+      <li className="productList-item" data-test="cart-stock-positions-changed-item">
+        <div className="product">
+          <figure className="product-column product-figure">
+            {getOrderSummaryItemImage(item)}
+          </figure>
+          <div className="product-column product-body">
+            <h4
+              className="product-title optimizedCheckout-contentPrimary body-medium"
+              data-test="cart-item-product-title"
+            >
+              {item.name}
+            </h4>
+            {item.options && item.options.length > 0 && (
+              <ul
+                className="product-options optimizedCheckout-contentSecondary sub-text-medium"
+                data-test="cart-item-product-options"
+              >
+                {item.options.map((option, index) => (
+                  <li className="product-option" data-test="cart-item-product-option" key={index}>
+                    {option.name} {option.value}
+                  </li>
+                ))}
+              </ul>
+            )}
+            <p
+              className="optimizedCheckout-contentSecondary sub-text-medium"
+              data-test="cart-item-quantity"
+            >
+              <TranslatedString data={{ quantity: item.quantity }} id="cart.qty_label" />
+            </p>
+          </div>
+          <div
+            className="cart-stock-position-details optimizedCheckout-contentSecondary sub-text-medium"
+            data-test="cart-item-stock-position"
+          >
+            {quantityOnHand > 0 && (
+              <span>
+                <TranslatedString
+                  data={{ count: quantityOnHand }}
+                  id="cart.ready_to_ship_count_text"
+                />
+              </span>
+            )}
+            {quantityBackordered > 0 && (
+              <span className="cart-stock-position-backorder">
+                {' '}
+                <TranslatedString
+                  data={{ count: quantityBackordered }}
+                  id="cart.backorder_count_text"
+                />
+              </span>
+            )}
+            {backorderMessage && (
+              <span className="cart-stock-position-backorder-message">{backorderMessage}</span>
+            )}
+          </div>
+        </div>
+      </li>
     );
 };
 
