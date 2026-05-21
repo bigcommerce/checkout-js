65e43f8ba7a28f3e351f6902c1e420b26de5c1c8
Peng Zhou <peng.zhou@bigcommerce.com>
2026-05-21 16:38:44 +1000

    feat(checkout): CHECKOUT-9821 Update loading B2B token

--- packages/contexts/src/capabilities/CapabilitiesProvider.tsx
+++ packages/contexts/src/capabilities/CapabilitiesProvider.tsx
@@ -1,8 +1,7 @@
 import { type CheckoutSelectors } from '@bigcommerce/checkout-sdk';
-import React, { type ReactElement, type ReactNode, useMemo } from 'react';
+import React, { type ReactElement, type ReactNode } from 'react';
 
 import CapabilitiesContext from './CapabilitiesContext';
-import { applyCapabilitiesOverride } from './capabilitiesOverride';
 import { defaultCapabilities } from './Capability';
 
 interface CapabilitiesProviderProps {
@@ -17,16 +16,8 @@ const CapabilitiesProvider = ({
     const capabilities =
         checkoutState.data.getConfig()?.checkoutSettings.capabilities ?? defaultCapabilities;
 
-    // TODO: CHECKOUT-9979: Remove the override logic
-    const overriddenCapabilities = useMemo(
-        () => applyCapabilitiesOverride(capabilities),
-        [capabilities],
-    );
-
     return (
-        <CapabilitiesContext.Provider value={overriddenCapabilities}>
-            {children}
-        </CapabilitiesContext.Provider>
+        <CapabilitiesContext.Provider value={capabilities}>{children}</CapabilitiesContext.Provider>
     );
 };
 
