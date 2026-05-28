f7597ccb53d1c5efa3880a353bffa769dfc78fce
Pavlo Khodakivskyi <pavlo.khodakivskyi@bigcommerce.com>
2026-05-28 14:53:45 +0300

    Fix build

--- packages/contexts/src/capabilities/Capability.ts
+++ packages/contexts/src/capabilities/Capability.ts
@@ -24,7 +24,7 @@ export const defaultCapabilities: Capabilities = {
     },
     payment: {
         paymentMethodFiltering: false,
-        b2bPaymentMethodFilter: false,
+        b2bPaymentMethodFilterType: null,
         poPaymentMethod: false,
         additionalPaymentNotes: false,
         excludeOfflineForInvoice: false,
