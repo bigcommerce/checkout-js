ecbccc03de366ba2d89f63d1d8328930c7d36747
PavlenkoM <mykhailo.pavlenko@bigcommerce.com>
2026-07-27 19:40:00 +0300

    feat(payment): add shipping options filtering callback for Stripe Link V2

--- packages/stripe-integration/src/stripe-ocs/StripeLinkV2Button.tsx
+++ packages/stripe-integration/src/stripe-ocs/StripeLinkV2Button.tsx
@@ -8,10 +8,19 @@ import {
     toResolvableComponent,
 } from '@bigcommerce/checkout/payment-integration-api';
 import { navigateToOrderConfirmation } from '@bigcommerce/checkout/utility';
+import { ShippingOption } from '@bigcommerce/checkout-sdk';
 
 const StripeLinkV2Button: FunctionComponent<CheckoutButtonProps> = (props) => {
+    const filterAvailableShippingOptions = (shippingOptions: ShippingOption[]) => {
+        // INFO: filter function can return a Promise or a value
+        return new Promise((resolve) => {
+            resolve(shippingOptions.filter((option) => option.type !== "shipping_pickupinstore"));
+        });
+        // return shippingOptions.filter((option) => option.type !== "shipping_pickupinstore");
+    };
     const additionalInitializationOptions = {
         onComplete: navigateToOrderConfirmation,
+        filterAvailableShippingOptions,
         loadingContainerId: 'checkout-app',
         methodId: 'optimized_checkout',
         gatewayId: 'stripeocs',
