3e3c2a5a7937d64dd89cd0fff323359f47133e83
Richa Patel <richa.patel@bigcommerce.com>
2026-08-20 11:36:20 +1000

    feat(checkout): CHECKOUT-10308 rename checkoutV2Theme to enhancedCheckoutThemeV1

--- packages/core/src/app/config/config.mock.ts
+++ packages/core/src/app/config/config.mock.ts
@@ -15,7 +15,7 @@ export function getStoreConfig(): StoreConfig {
             checkoutUserExperienceSettings: {
                 walletButtonsOnTop: false,
                 floatingLabelEnabled: false,
-                checkoutV2Theme: false,
+                enhancedCheckoutThemeV1: false,
             },
             enableOrderComments: true,
             enableTermsAndConditions: false,
