f84bb519788081df0ea4f268f464cf36ba6577a1
Max Yatsenko <max.yatsenko@bigcommerce.com>
2026-08-24 09:42:56 +1000

    fix(checkout): CHECKOUT-10186 Adjust step titles in enhanced theme v1 (#3264)
    * feat(checkout): CHECKOUT-10186 Add green check for completed steps
    
    * fix(checkout): CHECKOUT-10186 Do not show checkbox for active steps
    
    * fix(checkout): CHECKOUT-10186 resolve pr feedback

--- packages/ui/src/icon/IconCheck.tsx
+++ packages/ui/src/icon/IconCheck.tsx
@@ -4,7 +4,7 @@ import IconContainer, { type IconProps } from './IconContainer';
 
 const IconCheckSvg: FunctionComponent = () => (
     <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-        <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z" />
+        <path d="M9 14.76L5.53 11.29l-2.12 2.12L9 19 21 7l-2.12-2.12z" />
     </svg>
 );
 
