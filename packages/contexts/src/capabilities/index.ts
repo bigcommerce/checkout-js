e2782be5ac741417f0f556f8ea47ac5c738f3fd0
bc-maxy <max.yatsenko@bigcommerce.com>
2026-08-27 10:11:29 +1000

    chore(checkout): CHECKOUT-9842 Start experiment cleanup

--- packages/contexts/src/capabilities/index.ts
+++ packages/contexts/src/capabilities/index.ts
@@ -1,4 +1,3 @@
 export { default as CapabilitiesContext, useCapabilities } from './CapabilitiesContext';
-export { default as CapabilitiesProvider } from './CapabilitiesProvider';
 export { CapabilitiesProviderV2 } from './CapabilitiesProviderV2';
 export { defaultCapabilities } from './Capability';
--- packages/contexts/src/checkout/CheckoutProvider.tsx
+++ packages/contexts/src/checkout/CheckoutProvider.tsx
@@ -1,14 +1,7 @@
-import { type CheckoutSelectors, type CheckoutService } from '@bigcommerce/checkout-sdk';
-import React, {
-    type ReactElement,
-    type ReactNode,
-    useEffect,
-    useMemo,
-    useRef,
-    useState,
-} from 'react';
+import { type CheckoutService } from '@bigcommerce/checkout-sdk';
+import React, { type ReactNode, useMemo } from 'react';
 
-import { CapabilitiesProvider, CapabilitiesProviderV2 } from '../capabilities';
+import { CapabilitiesProviderV2 } from '../capabilities';
 
 import CheckoutContext from './CheckoutContext';
 import type ErrorLogger from './ErrorLogger';
@@ -17,23 +10,20 @@ export interface CheckoutProviderProps {
     checkoutService: CheckoutService;
     children: ReactNode;
     errorLogger?: ErrorLogger;
-    isCheckoutHookExperimentEnabled?: boolean;
 }
 
 const CheckoutProviderV2: React.FC<CheckoutProviderProps> = ({
     checkoutService,
     errorLogger,
     children,
-    isCheckoutHookExperimentEnabled,
 }) => {
     const contextValue = useMemo(
         () => ({
             checkoutService,
             checkoutState: checkoutService.getState(), // TODO: this can be removed once experiment is over
             errorLogger,
-            isCheckoutHookExperimentEnabled,
         }),
-        [checkoutService, errorLogger, isCheckoutHookExperimentEnabled],
+        [checkoutService, errorLogger],
     );
 
     return (
@@ -43,59 +33,8 @@ const CheckoutProviderV2: React.FC<CheckoutProviderProps> = ({
     );
 };
 
-const CheckoutProviderV1: React.FC<CheckoutProviderProps> = ({
-    checkoutService,
-    errorLogger,
-    children,
-}: CheckoutProviderProps): ReactElement => {
-    const [checkoutState, setCheckoutState] = useState<CheckoutSelectors>(() =>
-        checkoutService.getState(),
-    );
-    const unsubscribeRef = useRef<(() => void) | undefined>();
-
-    const contextValue = useMemo(
-        () => ({
-            checkoutService,
-            checkoutState,
-            errorLogger,
-        }),
-        [checkoutService, checkoutState, errorLogger],
-    );
-
-    useEffect(() => {
-        unsubscribeRef.current = checkoutService.subscribe((newCheckoutState) =>
-            setCheckoutState(newCheckoutState),
-        );
-
-        return () => {
-            if (unsubscribeRef.current) {
-                unsubscribeRef.current();
-                unsubscribeRef.current = undefined;
-            }
-        };
-    }, [checkoutService]);
-
-    return (
-        <CheckoutContext.Provider value={contextValue}>
-            <CapabilitiesProvider checkoutState={checkoutState}>{children}</CapabilitiesProvider>
-        </CheckoutContext.Provider>
-    );
-};
-
-const CheckoutProvider: React.FC<CheckoutProviderProps> = ({
-    isCheckoutHookExperimentEnabled,
-    ...props
-}) => {
-    if (isCheckoutHookExperimentEnabled) {
-        return (
-            <CheckoutProviderV2
-                isCheckoutHookExperimentEnabled={isCheckoutHookExperimentEnabled}
-                {...props}
-            />
-        );
-    }
-
-    return <CheckoutProviderV1 {...props} />;
+const CheckoutProvider: React.FC<CheckoutProviderProps> = ({ ...props }) => {
+    return <CheckoutProviderV2 {...props} />;
 };
 
 export default CheckoutProvider;
