599cbc26d507e31c679ccfa7a317db28e33740ef
bc-maxy <max.yatsenko@bigcommerce.com>
2026-08-27 10:35:06 +1000

    chore(checkout): CHECKOUT-9842 Fix test wrapper

--- packages/test-utils/src/TestWrapper.tsx
+++ packages/test-utils/src/TestWrapper.tsx
@@ -1,11 +1,10 @@
-import { type CheckoutSelectors, createCheckoutService } from '@bigcommerce/checkout-sdk/essential';
+import { createCheckoutService } from '@bigcommerce/checkout-sdk/essential';
 import { render, type RenderOptions } from '@testing-library/react';
 import React, { type ReactElement } from 'react';
 
 import {
-    CapabilitiesProvider,
+    CapabilitiesProviderV2,
     CheckoutProvider,
-    defaultCapabilities,
     LocaleContext,
     ThemeProvider,
 } from '@bigcommerce/checkout/contexts';
@@ -23,22 +22,11 @@ const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
 };
 
 const WithCapabilitiesProvider = ({ children }: { children: React.ReactNode }) => {
-    const storeConfig = getStoreConfig();
-    const configWithDefaultCapabilities = {
-        ...storeConfig,
-        checkoutSettings: {
-            ...storeConfig.checkoutSettings,
-            capabilities: defaultCapabilities,
-        },
-    };
-    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-    const checkoutState = {
-        data: {
-            getConfig: () => configWithDefaultCapabilities,
-        },
-    } as CheckoutSelectors;
-
-    return <CapabilitiesProvider checkoutState={checkoutState}>{children}</CapabilitiesProvider>;
+    return (
+        <CheckoutProvider checkoutService={createCheckoutService()}>
+            <CapabilitiesProviderV2>{children}</CapabilitiesProviderV2>
+        </CheckoutProvider>
+    );
 };
 
 const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>) =>
