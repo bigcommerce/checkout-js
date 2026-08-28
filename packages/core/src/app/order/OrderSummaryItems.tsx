ed268f725f62afca2b1a9797db3bbaad679ad9ea
Tom Arbesser-Rastburg <github@arbesser.org>
2026-08-29 00:50:12 +1000

    Fix cart items sorting

--- packages/core/src/app/order/OrderSummaryItems.tsx
+++ packages/core/src/app/order/OrderSummaryItems.tsx
@@ -130,12 +130,12 @@ const ProductList = ({
     const summaryItems = [
         ...items.physicalItems
             .slice()
-            .sort((item) => item.variantId)
+            .sort((a, b) => a.variantId - b.variantId)
             .map((item) => mapFromPhysical(item, bundleItemsMap)),
         ...items.giftCertificates.slice().map(mapFromGiftCertificate),
         ...items.digitalItems
             .slice()
-            .sort((item) => item.variantId)
+            .sort((a, b) => a.variantId - b.variantId)
             .map((item) => mapFromDigital(item, bundleItemsMap)),
         ...(items.customItems || []).map(mapFromCustom),
     ].slice(0, isExpanded ? undefined : collapsedLimit);
