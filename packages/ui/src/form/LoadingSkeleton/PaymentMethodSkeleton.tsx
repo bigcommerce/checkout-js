0b7990384643e34b067200a7d7aa71a218f02c67
bc-maxy <max.yatsenko@bigcommerce.com>
2026-09-10 14:54:46 +1000

    chore(checkout): CHECKOUT-10399 Use generic skeleton

--- packages/ui/src/form/LoadingSkeleton/PaymentMethodSkeleton.tsx
+++ packages/ui/src/form/LoadingSkeleton/PaymentMethodSkeleton.tsx
@@ -1,31 +1,9 @@
 import React, { type FunctionComponent } from 'react';
 
+import ChecklistSkeleton from './ChecklistSkeleton';
+
 export const PaymentMethodSkeleton: FunctionComponent = () => (
-    <div
-        aria-busy="true"
-        className="payment-method-skeleton"
-        data-test="payment-method-skeleton"
-        role="status"
-    >
-        <div className="payment-method-skeleton--row">
-            <div className="payment-method-skeleton--field-group payment-method-skeleton--field-group-large">
-                <div className="payment-method-skeleton--label" />
-                <div className="payment-method-skeleton--field-large" />
-            </div>
-            <div className="payment-method-skeleton--field-group payment-method-skeleton--field-group-small">
-                <div className="payment-method-skeleton--label" />
-                <div className="payment-method-skeleton--field-small" />
-            </div>
-        </div>
-        <div className="payment-method-skeleton--row">
-            <div className="payment-method-skeleton--field-group payment-method-skeleton--field-group-large">
-                <div className="payment-method-skeleton--label" />
-                <div className="payment-method-skeleton--field-large" />
-            </div>
-            <div className="payment-method-skeleton--field-group payment-method-skeleton--field-group-small">
-                <div className="payment-method-skeleton--label" />
-                <div className="payment-method-skeleton--field-small" />
-            </div>
-        </div>
+    <div aria-busy="true" data-test="payment-method-skeleton" role="status">
+        <ChecklistSkeleton additionalClassName="payment-method-skeleton" rows={2} />
     </div>
 );
